725f083df5645904f0e48da76aa60f2b59029893
Aleksei Lebedev <LastDragon@yandex.ru>
2020-11-04 16:53:34 +0400

    [migrator] Added override for `migrator`: new migrator support DI and nested directories.

---
+++ packages/migrator/src/Extenders/SmartMigrator.php
@@ -0,0 +1,58 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Migrator\Extenders;
+
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Database\ConnectionResolverInterface;
+use Illuminate\Database\Migrations\MigrationRepositoryInterface;
+use Illuminate\Database\Migrations\Migrator;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Str;
+use Symfony\Component\Finder\Finder;
+
+// TODO [laravel] [update] \Illuminate\Database\Migrations\Migrator
+
+/**
+ * Extends standard migrator.
+ *
+ * - Dependency Injection for migrations
+ * - Nested directories support
+ */
+class SmartMigrator extends Migrator {
+    protected ?Container $container;
+
+    public function __construct(
+        MigrationRepositoryInterface $repository,
+        ConnectionResolverInterface $resolver,
+        Filesystem $files,
+        Dispatcher $dispatcher = null,
+        Container $container = null
+    ) {
+        parent::__construct($repository, $resolver, $files, $dispatcher);
+
+        $this->container = $container;
+    }
+
+    // <editor-fold desc="\Illuminate\Database\Migrations\Migrator">
+    // =========================================================================
+    public function resolve($file) {
+        $class    = Str::studly(implode('_', array_slice(explode('_', $file), 4)));
+        $instance = $this->container
+            ? $this->container->make($class)
+            : new $class();
+
+        return $instance;
+    }
+
+    public function getMigrationFiles($paths) {
+        foreach ($paths as $path) {
+            foreach (Finder::create()->in($path)->directories() as $dir) {
+                $paths[] = $dir->getPathname();
+            }
+        }
+
+        return parent::getMigrationFiles($paths);
+    }
+    // </editor-fold>
+}
--- packages/migrator/src/Provider.php
+++ packages/migrator/src/Provider.php
@@ -4,8 +4,10 @@ namespace LastDragon_ru\LaraASP\Migrator;
 
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Database\Migrations\Migrator;
 use Illuminate\Support\ServiceProvider;
 use LastDragon_ru\LaraASP\Migrator\Extenders\RawMigrationCreator;
+use LastDragon_ru\LaraASP\Migrator\Extenders\SmartMigrator;
 
 class Provider extends ServiceProvider implements DeferrableProvider {
     // <editor-fold desc="\Illuminate\Support\ServiceProvider">
@@ -13,6 +15,7 @@ class Provider extends ServiceProvider implements DeferrableProvider {
     public function register() {
         parent::register();
 
+        $this->registerMigrator();
         $this->registerMigrationCreator();
     }
     // </editor-fold>
@@ -20,12 +23,18 @@ class Provider extends ServiceProvider implements DeferrableProvider {
     // <editor-fold desc="\Illuminate\Contracts\Support\DeferrableProvider">
     // =========================================================================
     public function provides() {
-        return [...parent::provides(), 'migration.creator'];
+        return [...parent::provides(), 'migrator', 'migration.creator'];
     }
     // </editor-fold>
 
     // <editor-fold desc="Functions">
     // =========================================================================
+    protected function registerMigrator() {
+        $this->app->singleton('migrator', function (Application $app) {
+            return new SmartMigrator($app['migration.repository'], $app['db'], $app['files'], $app['events'], $app);
+        });
+    }
+
     protected function registerMigrationCreator() {
         $this->app->singleton('migration.creator', function (Application $app) {
             return new RawMigrationCreator($app['files'], $app->basePath('stubs'));
