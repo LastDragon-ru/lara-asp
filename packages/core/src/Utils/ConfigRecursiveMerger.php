873b6a0de8f86be623f66dd06d0b0a0ae4b1a7c1
Aleksei Lebedev <LastDragon@yandex.ru>
2020-11-07 18:39:57 +0400

    [core] Added `ConfigRecursiveMerger` (merge two or more array-based configs)

---
+++ packages/core/src/Utils/ConfigRecursiveMerger.php
@@ -0,0 +1,80 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Core\Utils;
+
+use InvalidArgumentException;
+
+/**
+ * The merger for array-based configs.
+ */
+class ConfigRecursiveMerger {
+    private bool $strict = true;
+
+    public function __construct(bool $strict = true) {
+        $this->strict = $strict;
+    }
+
+    public function isStrict(): bool {
+        return $this->strict;
+    }
+
+    public function setStrict(bool $strict = true): self {
+        $this->strict = $strict;
+
+        return $this;
+    }
+
+    /**
+     * Merge two or more array-based configs.
+     *
+     * In strict mode (default) it will respect the structure of the target
+     * array, thus you cannot add any new keys, cannot replace existing
+     * scalar values by the array, and vice versa.
+     *
+     * @param array $target
+     * @param array ...$configs
+     *
+     * @return array
+     */
+    public function merge(array $target, array ...$configs): array {
+        foreach ($configs as $config) {
+            foreach ($config as $key => $value) {
+                // Only scalars/nulls and arrays of them allowed
+                if (!is_scalar($value) && !is_null($value) && !is_array($value)) {
+                    throw new InvalidArgumentException('Config may contain only scalar/null values and arrays of them.');
+                }
+
+                if (array_key_exists($key, $target)) {
+                    if (is_array($target[$key])) {
+                        // In strict mode $value must be an array
+                        if ($this->isStrict() && !is_array($value)) {
+                            throw new InvalidArgumentException('Array cannot be replaced by scalar/null value.');
+                        }
+
+                        if (is_string(key($target[$key]))) {
+                            $target[$key] = $this->merge($target[$key], (array) $value);
+                        } else {
+                            $target[$key] = array_values((array) $value);
+                        }
+                    } else {
+                        // In strict mode value cannot be replaced to array
+                        if ($this->isStrict() && is_array($value)) {
+                            throw new InvalidArgumentException('Scalar/null value cannot be replaced by array.');
+                        } else {
+                            $target[$key] = $value;
+                        }
+                    }
+                } else {
+                    // In strict mode $key must exists in $target
+                    if ($this->isStrict()) {
+                        throw new InvalidArgumentException('Unknown key.');
+                    } else {
+                        $target[$key] = $value;
+                    }
+                }
+            }
+        }
+
+        return $target;
+    }
+}
---
+++ packages/core/src/Utils/ConfigRecursiveMergerTest.php
@@ -0,0 +1,175 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Core\Utils;
+
+use Exception;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+/**
+ * @internal
+ * @coversDefaultClass \LastDragon_ru\LaraASP\Core\Utils\ConfigRecursiveMerger
+ */
+class ConfigRecursiveMergerTest extends TestCase {
+    // <editor-fold desc="Tests">
+    // =========================================================================
+    /**
+     * @covers ::merge
+     *
+     * @dataProvider dataProviderMerge
+     *
+     * @param array|\Exception $expected
+     * @param bool             $strict
+     * @param array            $config
+     * @param array            ...$configs
+     *
+     * @return void
+     */
+    public function testMerge($expected, bool $strict, array $config, array ...$configs): void {
+        if ($expected instanceof Exception) {
+            $this->expectExceptionObject($expected);
+        }
+
+        $merger = (new ConfigRecursiveMerger())->setStrict($strict);
+        $actual = $merger->merge($config, ... $configs);
+
+        $this->assertEquals($expected, $actual);
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="DataProviders">
+    // =========================================================================
+    public function dataProviderMerge(): array {
+        return [
+            'strict + array = ok'              => [
+                [
+                    'scalar' => 321,
+                    'null'   => null,
+                    'array'  => [1, 2, 3],
+                    'nested' => [
+                        'scalar' => 123,
+                        'null'   => 'qwe',
+                        'array'  => [1, 2, 3],
+                        'nested' => [
+                            'scalar' => 123,
+                            'null'   => null,
+                            'array'  => [3, 2, 1],
+                        ],
+                    ],
+                ],
+                true,
+                [
+                    'scalar' => 123,
+                    'null'   => null,
+                    'array'  => [1, 2, 3],
+                    'nested' => [
+                        'scalar' => 123,
+                        'null'   => null,
+                        'array'  => [1, 2, 3],
+                        'nested' => [
+                            'scalar' => 123,
+                            'null'   => null,
+                            'array'  => [1, 2, 3],
+                        ],
+                    ],
+                ],
+                [
+                    'scalar' => 321,
+                    'nested' => [
+                        'null'   => 'qwe',
+                        'nested' => [
+                            'array' => [3, 2, 1],
+                        ],
+                    ],
+                ],
+            ],
+            'strict + not scalar = error'      => [
+                new InvalidArgumentException('Config may contain only scalar/null values and arrays of them.'),
+                true,
+                [
+                    'scalar' => 123,
+                    'nested' => [
+                        'value' => 123,
+                    ],
+                ],
+                [
+                    'nested' => [
+                        'value' => new stdClass(),
+                    ],
+                ],
+            ],
+            'strict + unknown key = error'     => [
+                new InvalidArgumentException('Unknown key.'),
+                true,
+                [
+                    'scalar' => 123,
+                ],
+                [
+                    'unknown' => 123,
+                ],
+            ],
+            'strict + scalar => array = error' => [
+                new InvalidArgumentException('Scalar/null value cannot be replaced by array.'),
+                true,
+                [
+                    'value' => 123,
+                ],
+                [
+                    'value' => [1, 2, 3],
+                ],
+            ],
+            'strict + array => scalar = error' => [
+                new InvalidArgumentException('Array cannot be replaced by scalar/null value.'),
+                true,
+                [
+                    'value' => [1, 2, 3],
+                ],
+                [
+                    'value' => 123,
+                ],
+            ],
+            'unknown key'                      => [
+                [
+                    'scalar'  => 123,
+                    'unknown' => 123,
+                ],
+                false,
+                [
+                    'scalar' => 123,
+                ],
+                [
+                    'unknown' => 123,
+                ],
+            ],
+            'scalar => array'                  => [
+                [
+                    'value' => [1, 2, 3],
+                ],
+                false,
+                [
+                    'value' => 123,
+                ],
+                [
+                    'value' => [1, 2, 3],
+                ],
+            ],
+            'array => scalar'                  => [
+                [
+                    'value' => [321],
+                ],
+                false,
+                [
+                    'value' => [1, 2, 3],
+                ],
+                [
+                    'value' => 123,
+                ],
+                [
+                    'value' => [321],
+                ],
+            ],
+        ];
+    }
+    // </editor-fold>
+}
