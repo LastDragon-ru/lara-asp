9b3e6325e7992d83ec395ffff356fbf54530d66c
Aleksei Lebedev <LastDragon@yandex.ru>
2020-11-13 19:31:27 +0400

    [queue] Added `ConsoleKernelWithSchedule` (allows adding cron jobs in `Kernel`)

---
+++ packages/queue/src/Concerns/ConsoleKernelWithSchedule.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Queue\Concerns;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LastDragon_ru\LaraASP\Queue\CronableRegistrator;
+use LogicException;
+
+/**
+ * @property string[] $schedule {@link \LastDragon_ru\LaraASP\Queue\Contracts\Cronable} classes
+ * @mixin \Illuminate\Foundation\Console\Kernel
+ */
+trait ConsoleKernelWithSchedule {
+    protected function schedule(Schedule $schedule) {
+        if (!isset($this->schedule)) {
+            throw new LogicException('Class does not have $schedule property, please add it.');
+        }
+
+        $this->app->booted(function () use ($schedule) {
+            $registrator = $this->app->make(CronableRegistrator::class, [
+                'schedule' => $schedule,
+            ]);
+
+            foreach ((array) $this->schedule as $job) {
+                $registrator->register($job);
+            }
+        });
+    }
+}
