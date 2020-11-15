a628171f6977aac1b0c66dfeae85e3127d87eb51
Aleksei Lebedev <LastDragon@yandex.ru>
2020-11-15 16:24:02 +0400

    [core] Added `ClassUtils::getConstants()`.

---
+++ packages/core/src/Utils/ClassUtils.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Core\Utils;
+
+use ReflectionClass;
+
+class ClassUtils {
+    /**
+     * Get class public constants values.
+     *
+     * @param object|string $class
+     * @param string|null   $prefix
+     * @param bool          $recursive
+     *
+     * @return array
+     * @throws \ReflectionException
+     */
+    public static function getConstants($class, string $prefix = null, bool $recursive = true): array {
+        $values    = [];
+        $constants = (new ReflectionClass($class))->getReflectionConstants();
+
+        foreach ($constants as $constant) {
+            if (!$constant->isPublic()) {
+                continue;
+            }
+
+            if ($prefix && !str_starts_with($constant->getName(), $prefix)) {
+                continue;
+            }
+
+            if (!$recursive && $constant->getDeclaringClass()->name !== $class) {
+                continue;
+            }
+
+            $values[] = $constant->getValue();
+        }
+
+        return $values;
+    }
+}
---
+++ packages/core/src/Utils/ClassUtilsTest.php
@@ -0,0 +1,65 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Core\Utils;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ * @coversDefaultClass \LastDragon_ru\LaraASP\Core\Utils\ClassUtils
+ */
+class ClassUtilsTest extends TestCase {
+    // <editor-fold desc="Tests">
+    // =========================================================================
+    /**
+     * @covers ::getConstants
+     *
+     * @dataProvider dataProviderGetConstants
+     *
+     * @param array         $expected
+     * @param object|string $class
+     * @param string|null   $prefix
+     * @param bool          $recursive
+     *
+     * @return void
+     */
+    public function testGetConstants(array $expected, $class, ?string $prefix, bool $recursive): void {
+        $this->assertEqualsCanonicalizing($expected, ClassUtils::getConstants($class, $prefix, $recursive));
+    }
+
+    // </editor-fold>
+
+    // <editor-fold desc="DataProviders">
+    // =========================================================================
+    public function dataProviderGetConstants(): array {
+        return [
+            'class - without prefix - non recursive' => [['A', 'PUBLIC_A'], ClassUtilsTest__testGetConstants::class, null, false],
+            'class - with prefix - non recursive'    => [['PUBLIC_A'], ClassUtilsTest__testGetConstants::class, 'PUBLIC_', false],
+            'child - without prefix - non recursive' => [['PUBLIC_B'], ClassUtilsTest__testGetConstants__Child::class, null, false],
+            'child - without prefix - recursive'     => [['A', 'PUBLIC_A', 'PUBLIC_B'], ClassUtilsTest__testGetConstants__Child::class, null, true],
+            'child - with prefix - recursive'        => [['PUBLIC_A', 'PUBLIC_B'], ClassUtilsTest__testGetConstants__Child::class, 'PUBLIC_', true],
+        ];
+    }
+    // </editor-fold>
+}
+
+/**
+ * @internal
+ * @noinspection PhpMultipleClassesDeclarationsInOneFile
+ */
+class ClassUtilsTest__testGetConstants {
+    public const    A           = 'A';
+    public const    PUBLIC_A    = 'PUBLIC_A';
+    protected const PROTECTED_A = 'PROTECTED_A';
+    private const   PRIVATE_A   = 'PRIVATE_A';
+}
+
+/**
+ * @internal
+ * @noinspection PhpMultipleClassesDeclarationsInOneFile
+ */
+class ClassUtilsTest__testGetConstants__Child extends ClassUtilsTest__testGetConstants {
+    public const    PUBLIC_B    = 'PUBLIC_B';
+    protected const PROTECTED_B = 'PROTECTED_B';
+    private const   PRIVATE_B   = 'PRIVATE_B';
+}
