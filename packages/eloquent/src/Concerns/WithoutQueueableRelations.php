8ed4f0b6a648524232f8caf70a63e29997b13e8c
Aleksei Lebedev <LastDragon@yandex.ru>
2020-11-16 13:52:45 +0400

    [eloquent] Added `WithoutQueueableRelations` (removes relations from serialized models in queued objects)

---
+++ packages/eloquent/src/Concerns/WithoutQueueableRelations.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Eloquent\Concerns;
+
+/**
+ * @see https://github.com/laravel/framework/issues/26877
+ * @mixin \Illuminate\Database\Eloquent\Model
+ */
+trait WithoutQueueableRelations {
+    /**
+     * @return array
+     */
+    public function getQueueableRelations() {
+        // Usually, there are no reasons to save relations while serialization
+        // of `Queueable`, also, relations may create circular dependency, and
+        // deserialization will be failed.
+        return [];
+    }
+}
