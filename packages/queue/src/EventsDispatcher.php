a7c60a3b585397d294216e9be02302765e691638
Aleksei Lebedev <LastDragon@yandex.ru>
2020-11-22 12:21:07 +0400

    [queue] Added `EventsDispatcher` (fixes https://github.com/laravel/framework/issues/25272 that is required for `Listener`s).

---
+++ packages/queue/src/EventsDispatcher.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Queue;
+
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Queue\Factory;
+use Illuminate\Events\CallQueuedListener;
+use Illuminate\Events\Dispatcher;
+
+// TODO [laravel] [update] \Illuminate\Events\Dispatcher
+
+/**
+ * Fixed error dispatcher.
+ *
+ * It must be added to `bootstrap/app.php` by hand:
+ *
+ *      $app->singleton('events', \LastDragon_ru\LaraASP\Queue\EventsDispatcher::class);
+ *
+ * @see https://github.com/laravel/framework/issues/25272
+ */
+class EventsDispatcher extends Dispatcher {
+    public function __construct(Container $container = null) {
+        parent::__construct($container);
+
+        $this->setQueueResolver(function () {
+            return $this->container->make(Factory::class);
+        });
+    }
+
+    /**
+     * Create the listener and job for a queued listener.
+     *
+     * Laravel use {@link \ReflectionClass::newInstanceWithoutConstructor()} that
+     * make impossible create configurable event listeners so we need to fix it.
+     *
+     * @see          https://github.com/laravel/framework/issues/25272
+     * @noinspection PhpMissingReturnTypeInspection
+     * @inheritdoc
+     */
+    protected function createListenerAndJob($class, $method, $arguments) {
+        $listener = $this->container->make($class);
+
+        return [$listener, $this->propagateListenerOptions(
+            $listener, new CallQueuedListener($class, $method, $arguments)
+        )];
+    }
+}
