2f5f1e3a613e19044749b3332fa6026e7496d4da
Aleksei Lebedev <LastDragon@yandex.ru>
2020-11-22 15:18:51 +0400

    [queue] `CronJob` will extends `Job`.

--- packages/queue/src/Queueables/CronJob.php
+++ packages/queue/src/Queueables/CronJob.php
@@ -2,12 +2,8 @@
 
 namespace LastDragon_ru\LaraASP\Queue\Queueables;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\SerializesModels;
-use LastDragon_ru\LaraASP\Queue\Concerns\Configurable;
 use LastDragon_ru\LaraASP\Queue\Contracts\Cronable;
 
-abstract class CronJob implements ShouldQueue, Cronable {
-    use Queueable, SerializesModels, Configurable;
+abstract class CronJob extends Job implements Cronable {
+    // empty
 }
