98806385571dc203c04df7a02d394477b6f98ac4
Aleksei Lebedev <LastDragon@yandex.ru>
2020-11-23 16:30:45 +0400

    [core] Added `ProviderWithConfig` (allow recursive config merge for packages)

---
+++ packages/core/src/Concerns/ProviderWithConfig.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Core\Concerns;
+
+use Illuminate\Contracts\Foundation\CachesConfiguration;
+use LastDragon_ru\LaraASP\Core\Utils\ConfigRecursiveMerger;
+
+/**
+ * @mixin \Illuminate\Support\ServiceProvider
+ */
+trait ProviderWithConfig {
+    protected function loadConfigFrom(string $path, string $key, bool $strict = true): void {
+        if (!($this->app instanceof CachesConfiguration && $this->app->configurationIsCached())) {
+            $config = $this->app->make('config');
+
+            $config->set($key, (new ConfigRecursiveMerger($strict))->merge(
+                require $path, $config->get($key, [])
+            ));
+        }
+    }
+}
