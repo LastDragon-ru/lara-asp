d9d574d407d79ff6622a562763ee354d1a6efb11
Aleksei Lebedev <LastDragon@yandex.ru>
2020-12-06 15:38:55 +0400

    [testing] `FixRecentlyCreated` is independent of how the factory was created (the problem was in `configure()`, this method will be applied only after `Factory::new()` call).

--- packages/testing/src/Database/Eloquent/Factories/FixRecentlyCreated.php
+++ packages/testing/src/Database/Eloquent/Factories/FixRecentlyCreated.php
@@ -3,6 +3,7 @@
 namespace LastDragon_ru\LaraASP\Testing\Database\Eloquent\Factories;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 /**
  * After creating the model will have `wasRecentlyCreated = true`, in most
@@ -13,11 +14,15 @@ use Illuminate\Database\Eloquent\Model;
 trait FixRecentlyCreated {
     /**
      * @inheritdoc
-     * @noinspection PhpMissingReturnTypeInspection
      */
-    public function configure() {
-        return parent::configure()->afterCreating(function (Model $model) {
-            $model->wasRecentlyCreated = false;
-        });
+    protected function callAfterCreating(Collection $instances, ?Model $parent = null) {
+        $this->fixRecentlyCreated($instances);
+        parent::callAfterCreating($instances, $parent);
+    }
+
+    private function fixRecentlyCreated(Collection $instances): void {
+        foreach ($instances as $instance) {
+            $instance->wasRecentlyCreated = false;
+        }
     }
 }
