ea094cabef50dfea97f709b59c805ec89a021d83
Aleksei Lebedev <LastDragon@yandex.ru>
2020-12-06 18:09:07 +0400

    [testing] Tests for `ScalarStrictComparator`.

---
+++ packages/testing/src/Comparators/ScalarStrictComparatorTest.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Testing\Comparators;
+
+use PHPUnit\Framework\TestCase;
+use SebastianBergmann\Comparator\ComparisonFailure;
+
+/**
+ * @internal
+ * @coversDefaultClass \LastDragon_ru\LaraASP\Testing\Comparators\ScalarStrictComparator
+ */
+class ScalarStrictComparatorTest extends TestCase {
+    // <editor-fold desc="Tests">
+    // =========================================================================
+    /**
+     * @covers ::assertEquals
+     *
+     * @dataProvider dataProviderAssertEquals
+     *
+     * @param bool  $equals
+     * @param mixed $expected
+     * @param mixed $actual
+     *
+     * @return void
+     */
+    public function testAssertEquals(bool $equals, $expected, $actual): void {
+        if ($equals) {
+            $this->assertTrue(true);
+        } else {
+            $this->expectException(ComparisonFailure::class);
+        }
+
+        (new ScalarStrictComparator())->assertEquals($expected, $actual);
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="DataProviders">
+    // =========================================================================
+    public function dataProviderAssertEquals(): array {
+        return [
+            'int'         => [true, 1, 1],
+            'bool'        => [true, true, true],
+            'weird php 1' => [false, '', false],
+            'weird php 2' => [false, null, false],
+            'weird php 3' => [false, 0, false],
+            'weird php 4' => [false, 0, null],
+            'weird php 5' => [false, null, ''],
+        ];
+    }
+    // </editor-fold>
+}
