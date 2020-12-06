01aebbc68bda09b2750578d0a7e06cfc28ec3bdf
Aleksei Lebedev <LastDragon@yandex.ru>
2020-12-06 18:54:33 +0400

    [eloquent] `Model` marked as `abstract`.

--- packages/eloquent/src/Model.php
+++ packages/eloquent/src/Model.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model as EloquentModel;
 use LastDragon_ru\LaraASP\Eloquent\Concerns\SaveOrThrow;
 use LastDragon_ru\LaraASP\Eloquent\Concerns\WithoutQueueableRelations;
 
-class Model extends EloquentModel {
+abstract class Model extends EloquentModel {
     use SaveOrThrow, WithoutQueueableRelations;
 }
