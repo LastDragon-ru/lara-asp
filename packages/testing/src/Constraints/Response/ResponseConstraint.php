e87d5f5c7f1f21af5026f1bc11bf182925c84ef1
Aleksei Lebedev <LastDragon@yandex.ru>
2021-01-13 15:25:47 +0400

    [testing] Fixed type of `ResponseConstraint::$constraint`.

--- packages/testing/src/Constraints/Response/ResponseConstraint.php
+++ packages/testing/src/Constraints/Response/ResponseConstraint.php
@@ -4,12 +4,13 @@ namespace LastDragon_ru\LaraASP\Testing\Constraints\Response;
 
 use LastDragon_ru\LaraASP\Testing\Providers\CompositeExpectedImpl;
 use LastDragon_ru\LaraASP\Testing\Providers\CompositeExpectedInterface;
+use PHPUnit\Framework\Constraint\BinaryOperator;
 use PHPUnit\Framework\Constraint\LogicalAnd;
 
 class ResponseConstraint extends Constraint implements CompositeExpectedInterface {
     use CompositeExpectedImpl;
 
-    protected LogicalAnd $constraint;
+    protected BinaryOperator $constraint;
 
     public function __construct(Constraint ...$constraints) {
         $this->constraint = LogicalAnd::fromConstraints(...$constraints);
