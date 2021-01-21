a2a1db50a59c100608096149ca2d04f065a3dff6
Aleksei Lebedev <LastDragon@yandex.ru>
2021-01-21 18:36:43 +0400

    [spa] Added `Angular\Url`.

---
+++ packages/spa/src/Angular/Url.php
@@ -0,0 +1,129 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Spa\Angular;
+
+use GuzzleHttp\Psr7\Utils;
+use InvalidArgumentException;
+use JetBrains\PhpStorm\Pure;
+use function array_diff_key;
+use function array_fill_keys;
+use function array_keys;
+use function array_map;
+use function explode;
+use function http_build_query;
+use function implode;
+use function is_array;
+use function is_bool;
+use function is_float;
+use function mb_substr;
+use function parse_url;
+use function sprintf;
+use function str_replace;
+use function str_starts_with;
+use const PHP_QUERY_RFC3986;
+use const PHP_URL_PATH;
+
+class Url {
+    private string $template;
+    /**
+     * @var string[]
+     */
+    private array $parameters;
+
+    public function __construct(string $template) {
+        $this->template   = $template;
+        $this->parameters = $this->extract($template);
+    }
+
+    #[Pure]
+    public function getTemplate(): string {
+        return $this->template;
+    }
+
+    /**
+     * @return string[]
+     */
+    #[Pure]
+    public function getParameters(): array {
+        return $this->parameters;
+    }
+
+    #[Pure]
+    public function build(array $parameters = []): string {
+        // Check
+        $params = $this->getParameters();
+        $missed = array_diff_key(array_fill_keys($params, null), $parameters);
+
+        if ($missed) {
+            throw new InvalidArgumentException(
+                sprintf('Url requires the following parameters: %s.', implode(', ', array_keys($missed)))
+            );
+        }
+
+        // Replace params
+        $url        = Utils::uriFor($this->getTemplate());
+        $parameters = $this->serialize($parameters);
+
+        foreach ($params as $param) {
+            $url = $url->withPath(str_replace(":{$param}", $parameters[$param], $url->getPath()));
+
+            unset($parameters[$param]);
+        }
+
+        // Add query params
+        if ($parameters) {
+            $query = http_build_query($parameters, '', '&', PHP_QUERY_RFC3986);
+            $query = preg_replace('/%5B\d+%5D/ui', '', $query);
+
+            if ($url->getQuery()) {
+                $url = $url->withQuery("{$url->getQuery()}&{$query}");
+            } else {
+                $url = $url->withQuery($query);
+            }
+        }
+
+        // Return
+        return (string) $url;
+    }
+
+    /**
+     * @param string $template
+     *
+     * @return string[]
+     */
+    #[Pure]
+    private function extract(string $template): array {
+        $names = [];
+        $parts = explode('/', parse_url($template, PHP_URL_PATH));
+
+        foreach ($parts as $part) {
+            if (str_starts_with($part, ':')) {
+                $names[] = mb_substr($part, 1);
+            }
+        }
+
+        return $names;
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @return string|array|null
+     */
+    #[Pure]
+    private function serialize($value) {
+        if (is_float($value)) {
+            $value = str_replace(',', '.', (string) $value);
+        } elseif (is_bool($value)) {
+            $value = (string) ((int) $value);
+        } elseif (is_array($value)) {
+            $value = array_map(function ($value) {
+                return $this->serialize($value);
+            }, $value);
+        } else {
+            $value = (string) $value;
+        }
+
+        return $value;
+    }
+}
---
+++ packages/spa/src/Angular/UrlTest.php
@@ -0,0 +1,119 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Spa\Angular;
+
+use Exception;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ * @coversDefaultClass \LastDragon_ru\LaraASP\Spa\Angular\Url
+ */
+class UrlTest extends TestCase {
+    // <editor-fold desc="Tests">
+    // =========================================================================
+    /**
+     * @covers ::__construct()
+     * @covers ::extract
+     *
+     * @dataProvider dataProviderConstruct
+     *
+     * @param array  $expected
+     * @param string $template
+     *
+     * @return void
+     */
+    public function testConstruct(array $expected, string $template): void {
+        $url = new Url($template);
+
+        $this->assertEquals($template, $url->getTemplate());
+        $this->assertEquals($expected, $url->getParameters());
+    }
+
+    /**
+     * @covers ::build
+     *
+     * @dataProvider dataProviderBuild
+     *
+     * @param string|\Exception $expected
+     * @param string            $template
+     * @param array             $parameters
+     *
+     * @return void
+     */
+    public function testBuild($expected, string $template, array $parameters): void {
+        if ($expected instanceof Exception) {
+            $this->expectExceptionObject($expected);
+        }
+
+        $this->assertEquals($expected, (new Url($template))->build($parameters));
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="DataProviders">
+    // =========================================================================
+    public function dataProviderConstruct(): array {
+        return [
+            'url without params'  => [[], 'http://example.com/path?:id=1'],
+            'url with params'     => [['to', 'id'], 'http://example.com/path/:to/item/:id'],
+            'path without params' => [[], 'path/to/item/id'],
+            'path with params'    => [['to', 'id'], 'path/:to/item/:id'],
+        ];
+    }
+
+    public function dataProviderBuild(): array {
+        return [
+            'url without params without data'       => ['http://example.com/path', 'http://example.com/path', []],
+            'url without params with data'          => ['http://example.com/path?int=123&null=&true=1&false=0&float=1.23&array=1&array=2&array=3&string=value', 'http://example.com/path', [
+                'int'    => 123,
+                'null'   => null,
+                'true'   => true,
+                'false'  => false,
+                'float'  => 1.23,
+                'array'  => [1, 2, 3],
+                'string' => 'value',
+            ]],
+            'url with params without data'          => [
+                new InvalidArgumentException('Url requires the following parameters: to, id.'),
+                'http://example.com/path/:to/item/:id',
+                [],
+            ],
+            'url with params with data'             => ['http://example.com/path/1/item/2?int=123', 'http://example.com/path/:to/item/:id', [
+                'to'  => 1,
+                'id'  => 2,
+                'int' => 123,
+            ]],
+            'url with params with data with query'  => ['http://example.com/path/1/item/2?value=123&int=123', 'http://example.com/path/:to/item/:id?value=123', [
+                'to'  => 1,
+                'id'  => 2,
+                'int' => 123,
+            ]],
+            'path without params without data'      => ["path/to/item/id?int=123&null=&true=1&false=0&float=1.23&array=1&array=2&array=3&string=value", 'path/to/item/id', [
+                'int'    => 123,
+                'null'   => '',
+                'true'   => true,
+                'false'  => false,
+                'float'  => 1.23,
+                'array'  => [1, 2, 3],
+                'string' => 'value',
+            ]],
+            'path with params with data'            => ["/path/1/item/2?int=123", '/path/:to/item/:id', [
+                'to'  => 1,
+                'id'  => 2,
+                'int' => 123,
+            ]],
+            'path with params with data with query' => ["/path/1/item/2?value=123&int=123", '/path/:to/item/:id?value=123', [
+                'to'  => 1,
+                'id'  => 2,
+                'int' => 123,
+            ]],
+            'path with params without data'         => [
+                new InvalidArgumentException('Url requires the following parameters: to, id.'),
+                'path/:to/item/:id',
+                [],
+            ],
+        ];
+    }
+    // </editor-fold>
+}
