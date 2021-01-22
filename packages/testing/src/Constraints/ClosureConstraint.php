767c0ed548614e798a8debcbd56288c540c39a72
Aleksei Lebedev <LastDragon@yandex.ru>
2021-01-22 13:31:17 +0400

    [testing] Added `ClosureConstraint` that allow use Closure as constraint.

---
+++ packages/testing/src/Constraints/ClosureConstraint.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Testing\Constraints;
+
+use Closure;
+use PHPUnit\Framework\Constraint\Constraint;
+
+class ClosureConstraint extends Constraint {
+    private Closure $closure;
+
+    public function __construct(Closure $closure) {
+        $this->closure = $closure;
+    }
+
+    protected function getClosure(): Closure {
+        return $this->closure;
+    }
+
+    protected function matches($other): bool {
+        return $this->getClosure()($other);
+    }
+
+    public function toString(): string {
+        return 'matches closure';
+    }
+}
