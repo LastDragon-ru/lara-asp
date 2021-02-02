4b75fa01c901d1f853bfa9707b7a2df677d2cf3b
Aleksei Lebedev <LastDragon@yandex.ru>
2021-02-02 19:32:50 +0400

    [spa] Added `ResourceCollection` and `PaginatedCollection` for testing responses that contain collection of items and paginated collection of items.

---
+++ packages/spa/src/Testing/Http/Resources/PaginatedCollectionTest.php
@@ -0,0 +1,171 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Spa\Testing\Http\Resources;
+
+use LastDragon_ru\LaraASP\Testing\Constraints\Json\JsonMatchesSchema;
+use LastDragon_ru\LaraASP\Testing\Utils\WithTestData;
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ * @coversDefaultClass \LastDragon_ru\LaraASP\Spa\Testing\Http\Resources\PaginatedCollection
+ */
+class PaginatedCollectionTest extends TestCase {
+    use WithTestData;
+
+    // <editor-fold desc="Tests">
+    // =========================================================================
+    /**
+     * @coversNothing
+     *
+     * @dataProvider dataProviderSchema
+     *
+     * @param bool  $expected
+     * @param array $json
+     */
+    public function testSchema(bool $expected, array $json): void {
+        $schema     = new PaginatedCollection(static::class);
+        $constraint = new JsonMatchesSchema($schema);
+        $message    = '';
+        $actual     = null;
+
+        try {
+            $actual = $constraint->evaluate($json);
+            $actual = true;
+        } catch (ExpectationFailedException $exception) {
+            $message = $exception->getMessage();
+        }
+
+        $this->assertEquals($expected, $actual, $message);
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="DataProviders">
+    // =========================================================================
+    public function dataProviderSchema(): array {
+        return [
+            'object (invalid)'                 => [
+                false,
+                [
+                    'value' => 123,
+                ],
+            ],
+            'array (invalid)'                  => [
+                false,
+                [
+                    [
+                        'value' => 123,
+                    ],
+                ],
+            ],
+            'paginated (length aware)'         => [
+                true,
+                [
+                    'items' => [
+                        [
+                            'value' => 123,
+                        ],
+                        [
+                            'value' => 123,
+                        ],
+                    ],
+                    'meta'  => [
+                        'current_page' => 1,
+                        'last_page'    => 5,
+                        'per_page'     => 2,
+                        'total'        => 3,
+                        'from'         => 26,
+                        'to'           => 27,
+                    ],
+                ],
+            ],
+            'paginated (length aware + empty)' => [
+                true,
+                [
+                    'items' => [],
+                    'meta'  => [
+                        'current_page' => 1,
+                        'last_page'    => 5,
+                        'per_page'     => 2,
+                        'total'        => 3,
+                        'from'         => 26,
+                        'to'           => 27,
+                    ],
+                ],
+            ],
+            'paginated (simple)'               => [
+                true,
+                [
+                    'items' => [
+                        [
+                            'value' => 123,
+                        ],
+                        [
+                            'value' => 123,
+                        ],
+                    ],
+                    'meta'  => [
+                        'current_page' => 1,
+                        'last_page'    => null,
+                        'per_page'     => 2,
+                        'total'        => null,
+                        'from'         => 1,
+                        'to'           => 27,
+                    ],
+                ],
+            ],
+            'paginated (simple + empty)'       => [
+                true,
+                [
+                    'items' => [],
+                    'meta'  => [
+                        'current_page' => 1,
+                        'last_page'    => null,
+                        'per_page'     => 2,
+                        'total'        => null,
+                        'from'         => 1,
+                        'to'           => 27,
+                    ],
+                ],
+            ],
+            'paginated (invalid items)'        => [
+                false,
+                [
+                    'items' => [
+                        'value' => 123,
+                    ],
+                    'meta'  => [
+                        'current_page' => 1,
+                        'last_page'    => 5,
+                        'per_page'     => 2,
+                        'total'        => 3,
+                        'from'         => 26,
+                        'to'           => 27,
+                    ],
+                ],
+            ],
+            'paginated (invalid meta)'         => [
+                false,
+                [
+                    'items' => [
+                        [
+                            'value' => 123,
+                        ],
+                        [
+                            'value' => 123,
+                        ],
+                    ],
+                    'meta'  => [
+                        'current_page' => 1,
+                        'per_page'     => 2,
+                        'total'        => 3,
+                        'from'         => 26,
+                        'to'           => 27,
+                    ],
+                ],
+            ],
+        ];
+    }
+    // </editor-fold>
+}
---
+++ packages/spa/src/Testing/Http/Resources/ResourceCollection.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Spa\Testing\Http\Resources;
+
+use LastDragon_ru\LaraASP\Testing\Constraints\Json\JsonSchemaWrapper;
+
+class ResourceCollection extends JsonSchemaWrapper {
+    // empty
+}
