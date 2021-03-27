4f5326578af94dc1c8c59e55c761ae89a01e148f
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-03-27 21:48:10 +0400

    [graphql] Enum support.

---
+++ packages/graphql/src/Helpers/EnumHelper.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\GraphQL\Helpers;
+
+use GraphQL\Type\Definition\EnumType;
+use LastDragon_ru\LaraASP\Core\Enum;
+use phpDocumentor\Reflection\DocBlockFactory;
+use ReflectionClass;
+use ReflectionMethod;
+
+class EnumHelper extends Enum {
+    /**
+     * Converts {@link \LastDragon_ru\LaraASP\Core\Enum} into GraphQL enum that
+     * can be reqistered in {@link \Nuwave\Lighthouse\Schema\TypeRegistry}.
+     *
+     * @see https://lighthouse-php.com/master/the-basics/types.html#enum
+     *
+     * @param class-string<\LastDragon_ru\LaraASP\Core\Enum> $enum
+     */
+    public static function getType(string $enum, ?string $name = null): EnumType {
+        $class      = new ReflectionClass($enum);
+        $definition = [
+            'name'        => $name ?: $class->getShortName(),
+            'description' => static::description($class),
+            'values'      => [],
+        ];
+
+        $enum::lookup(static function (ReflectionMethod $method) use (&$definition): void {
+            $definition['values'][$method->getName()] = [
+                'value'       => $method->invoke(null)->getValue(),
+                'description' => static::description($method),
+            ];
+        });
+
+        return new EnumType($definition);
+    }
+
+    protected static function description(ReflectionClass|ReflectionMethod $object): ?string {
+        return $object->getDocComment()
+            ? (string) DocBlockFactory::createInstance()->create($object)->getDescription()
+            : null;
+    }
+}
---
+++ packages/graphql/src/Helpers/EnumHelperTest.php
@@ -0,0 +1,94 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\GraphQL\Helpers;
+
+use LastDragon_ru\LaraASP\Core\Enum;
+use LastDragon_ru\LaraASP\GraphQL\Testing\TestCase;
+use Nuwave\Lighthouse\Schema\TypeRegistry;
+
+/**
+ * @internal
+ * @coversDefaultClass \LastDragon_ru\LaraASP\GraphQL\Helpers\EnumHelper
+ */
+class EnumHelperTest extends TestCase {
+    /**
+     * @covers ::getType
+     */
+    public function testGetType(): void {
+        $a        = EnumHelper::getType(EnumHelperTest__A::class);
+        $b        = EnumHelper::getType(EnumHelperTest__B::class, 'B');
+        $registry = $this->app->make(TypeRegistry::class);
+
+        $registry->register($a);
+        $registry->register($b);
+
+        $expected = $this->getTestData()->content('.graphql');
+        $actual   = $this->getSchema(
+            /** @lang GraphQL */
+            <<<'GRAPHQL'
+            type Query {
+              test(a: EnumHelperTest__A, b: B): ID! @all
+            }
+            GRAPHQL,
+        );
+
+        $this->assertEquals($expected, $actual);
+    }
+}
+
+// @phpcs:disable PSR1.Classes.ClassDeclaration.MultipleClasses
+// @phpcs:disable Squiz.Classes.ValidClassName.NotCamelCaps
+
+/**
+ * @internal
+ * @noinspection PhpMultipleClassesDeclarationsInOneFile
+ */
+class EnumHelperTest__A extends Enum {
+    public static function a(): static {
+        return static::make(__FUNCTION__);
+    }
+
+    /**
+     * Summary summary summary summary summary summary. Summary summary summary
+     * summary summary summary. Summary summary summary summary summary summary.
+     * Summary summary summary summary summary summary.
+     *
+     * Description description description description description. Description
+     * description description description description Description description
+     * description description description.
+     *
+     * Description description description description description. Description
+     * description description description description Description description
+     * description description description.
+     */
+    public static function b(): static {
+        return static::make(__FUNCTION__);
+    }
+}
+
+/**
+ * Summary summary summary summary summary summary. Summary summary summary
+ * summary summary summary. Summary summary summary summary summary summary.
+ * Summary summary summary summary summary summary.
+ *
+ * Description description description description description. Description
+ * description description description description Description description
+ * description description description.
+ *
+ * Description description description description description. Description
+ * description description description description Description description
+ * description description description.
+ *
+ * @internal
+ *
+ * @noinspection PhpMultipleClassesDeclarationsInOneFile
+ */
+class EnumHelperTest__B extends Enum {
+    public static function a(): static {
+        return static::make(__FUNCTION__);
+    }
+
+    public static function b(): static {
+        return static::make(__FUNCTION__);
+    }
+}
