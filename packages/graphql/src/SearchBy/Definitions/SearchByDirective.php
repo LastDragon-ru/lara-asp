c2b5249ca646d83647d5c844bbd2464a68e1d974
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-04-10 19:45:47 +0400

    Directives moved into Definitions namespace.

--- packages/graphql/src/SearchBy/Definitions/SearchByDirective.php
+++ packages/graphql/src/SearchBy/Definitions/SearchByDirective.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace LastDragon_ru\LaraASP\GraphQL\SearchBy\Directive;
+namespace LastDragon_ru\LaraASP\GraphQL\SearchBy\Definitions;
 
 use LastDragon_ru\LaraASP\GraphQL\SearchBy\Directive;
 
