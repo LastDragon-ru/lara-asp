3c673e146d8dde38a55f8c02d2f0d377a724c36c
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-04-25 16:25:34 +0400

    [testing] Added JsonFragmentMatchesSchema constraint.

---
+++ packages/testing/src/Constraints/Json/JsonFragmentMatchesSchema.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Testing\Constraints\Json;
+
+use Illuminate\Support\Arr;
+use LastDragon_ru\LaraASP\Testing\Utils\Args;
+
+use function sprintf;
+
+/**
+ * Check that JSON fragment matches schema (draft-07 and draft-06).
+ *
+ * @see https://json-schema.org/
+ * @see https://github.com/opis/json-schema
+ */
+class JsonFragmentMatchesSchema extends JsonMatchesSchema {
+    public function __construct(
+        protected string $path,
+        JsonSchema $schema,
+    ) {
+        parent::__construct($schema);
+    }
+
+    // <editor-fold desc="\PHPUnit\Framework\Constraint\Constraint">
+    // =========================================================================
+    /**
+     * @inheritdoc
+     */
+    public function evaluate($other, string $description = '', bool $returnResult = false): ?bool {
+        return parent::evaluate(
+            Arr::get(Args::getJson($other, true), $this->path),
+            $description,
+            $returnResult,
+        );
+    }
+
+    public function toString(): string {
+        return sprintf(
+            'fragment "%s" matches JSON schema',
+            $this->path,
+        );
+    }
+    // </editor-fold>
+}
