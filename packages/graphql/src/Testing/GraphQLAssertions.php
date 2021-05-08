40095f51aa9479810c17b968ee6b5eb8ffe73f04
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-05-08 11:50:18 +0400

    [graphql] Better assertions for testing.

--- packages/graphql/src/Testing/GraphQLAssertions.php
+++ packages/graphql/src/Testing/GraphQLAssertions.php
@@ -4,9 +4,11 @@ namespace LastDragon_ru\LaraASP\GraphQL\Testing;
 
 use GraphQL\Type\Schema;
 use GraphQL\Utils\SchemaPrinter;
+use Illuminate\Contracts\Config\Repository;
 use LastDragon_ru\LaraASP\Testing\Utils\Args;
-use Nuwave\Lighthouse\GraphQL;
+use Nuwave\Lighthouse\Schema\SchemaBuilder;
 use Nuwave\Lighthouse\Schema\Source\SchemaSourceProvider;
+use Nuwave\Lighthouse\Schema\Source\SchemaStitcher;
 use Nuwave\Lighthouse\Testing\MocksResolvers;
 use Nuwave\Lighthouse\Testing\TestSchemaProvider;
 use SplFileInfo;
@@ -17,41 +19,76 @@ use SplFileInfo;
 trait GraphQLAssertions {
     use MocksResolvers;
 
+    // <editor-fold desc="Assertions">
+    // =========================================================================
     /**
      * Compares two GraphQL schemas.
      */
     public function assertGraphQLSchemaEquals(
         SplFileInfo|string $expected,
-        SplFileInfo|string $schema,
+        Schema|SplFileInfo|string $schema,
         string $message = '',
     ): void {
         $this->assertEquals(
             Args::content($expected),
-            $this->serializeGraphQLSchema($schema),
+            $this->printGraphQLSchema($schema),
             $message,
         );
     }
 
-    protected function useGraphQLSchema(SplFileInfo|string $schema): static {
+    /**
+     * Compares GraphQL schema with default (application) schema.
+     */
+    public function assertDefaultGraphQLSchemaEquals(
+        SplFileInfo|string $expected,
+        string $message = '',
+    ): void {
+        $this->assertGraphQLSchemaEquals(
+            $expected,
+            $this->getDefaultGraphQLSchema(),
+            $message,
+        );
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="Helpers">
+    // =========================================================================
+    protected function getGraphQLSchema(SplFileInfo|string $schema): Schema {
         $schema = Args::content($schema);
 
         $this->app->bind(SchemaSourceProvider::class, static function () use ($schema): SchemaSourceProvider {
             return new TestSchemaProvider($schema);
         });
 
-        return $this;
-    }
-
-    protected function getGraphQLSchema(SplFileInfo|string $schema): Schema {
-        $this->useGraphQLSchema($schema);
-
-        $graphql = $this->app->make(GraphQL::class);
-        $schema  = $graphql->prepSchema();
+        $graphql = $this->app->make(SchemaBuilder::class);
+        $schema  = $graphql->schema();
 
         return $schema;
     }
 
-    protected function serializeGraphQLSchema(SplFileInfo|string $schema): string {
-        return SchemaPrinter::doPrint($this->getGraphQLSchema($schema));
+    protected function getDefaultGraphQLSchema(): Schema {
+        $this->app->bind(SchemaSourceProvider::class, function (): SchemaSourceProvider {
+            return new SchemaStitcher(
+                $this->app->make(Repository::class)->get('lighthouse.schema.register', ''),
+            );
+        });
+
+        $graphql = $this->app->make(SchemaBuilder::class);
+        $schema  = $graphql->schema();
+
+        return $schema;
     }
+
+    protected function printGraphQLSchema(Schema|SplFileInfo|string $schema): string {
+        if (!($schema instanceof Schema)) {
+            $schema = $this->getGraphQLSchema($schema);
+        }
+
+        return SchemaPrinter::doPrint($schema);
+    }
+
+    protected function printDefaultGraphQLSchema(): string {
+        return $this->printGraphQLSchema($this->getDefaultGraphQLSchema());
+    }
+    // </editor-fold>
 }
