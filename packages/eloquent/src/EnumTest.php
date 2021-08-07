5eccbd5056d173f2a03fe0f36465607e645d0291
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-08-07 14:20:29 +0400

    Access to an undefined property LastDragon_ru\LaraASP\Eloquent\EnumTest_Model::$value.

--- packages/eloquent/src/EnumTest.php
+++ packages/eloquent/src/EnumTest.php
@@ -58,6 +58,8 @@ class EnumTest_Enum extends Enum {
 /**
  * @internal
  * @noinspection PhpMultipleClassesDeclarationsInOneFile
+ *
+ * @property string $value
  */
 class EnumTest_Model extends TestObject {
     /**
