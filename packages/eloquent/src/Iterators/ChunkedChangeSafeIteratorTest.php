53b1b0b75f8ef98de208c6b6e5bdfb75fd64b00f
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-08-08 18:31:55 +0400

    Parameter #1 $callback of static method Closure::fromCallable() expects callable(): mixed, Mockery\MockInterface given.

--- packages/eloquent/src/Iterators/ChunkedChangeSafeIteratorTest.php
+++ packages/eloquent/src/Iterators/ChunkedChangeSafeIteratorTest.php
@@ -38,9 +38,16 @@ class ChunkedChangeSafeIteratorTest extends TestCase {
         $count     = count($log);
         $iterator  = (new ChunkedChangeSafeIterator($query))
             ->setChunkSize(2)
-            ->onBeforeChunk(Closure::fromCallable($spyBefore))
-            ->onAfterChunk(Closure::fromCallable($spyAfter));
-        $actual    = [];
+            ->onBeforeChunk(
+                /** @phpstan-ignore-next-line False positive, see https://github.com/phpstan/phpstan-mockery/issues/34 */
+                Closure::fromCallable($spyBefore),
+            )
+            ->onAfterChunk(
+                /** @phpstan-ignore-next-line False positive, see https://github.com/phpstan/phpstan-mockery/issues/34 */
+                Closure::fromCallable($spyAfter),
+            );
+
+        $actual = [];
 
         foreach ($iterator as $model) {
             $actual[] = $model;
