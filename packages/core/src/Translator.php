c9e1e5d0fc36e11686c07fc7b712474c7d3cd83d
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-08-28 10:56:01 +0400

    feat(core): Package `Translator` allows to specify default translation (will be used if the translation string doesn't exist).

--- packages/core/src/Translator.php
+++ packages/core/src/Translator.php
@@ -2,8 +2,13 @@
 
 namespace LastDragon_ru\LaraASP\Core;
 
+use Closure;
 use Countable;
 use Illuminate\Contracts\Translation\Translator as TranslatorContract;
+
+use function array_splice;
+use function array_values;
+use function end;
 
 /**
  * Special wrapper around Translator to help translate package's messages.
@@ -20,20 +25,26 @@ abstract class Translator implements TranslatorContract {
     /**
      * @inheritDoc
      *
-     * @param array<mixed> $replace
+     * @param array<string>|string $key
+     * @param array<mixed>         $replace
      */
     public function get($key, array $replace = [], $locale = null) {
-        return $this->translator->get($this->key($key), $replace, $locale);
+        return $this->translate($key, function (string $key) use ($replace, $locale): string {
+            return $this->translator->get($this->key($key), $replace, $locale);
+        });
     }
 
     /**
      * @inheritDoc
      *
+     * @param array<string>|string       $key
      * @param Countable|int|array<mixed> $number
      * @param array<mixed>               $replace
      */
     public function choice($key, $number, array $replace = [], $locale = null): string {
-        return $this->translator->choice($this->key($key), $number, $replace, $locale);
+        return $this->translate($key, function (string $key) use ($number, $replace, $locale): string {
+            return $this->translator->choice($this->key($key), $number, $replace, $locale);
+        });
     }
 
     public function getLocale(): string {
@@ -51,5 +62,29 @@ abstract class Translator implements TranslatorContract {
         return $this->group
             ? "{$this->package}::{$this->group}.{$key}"
             : "{$this->package}::{$key}";
+    }
+
+    /**
+     * @param array<string>|string    $variants
+     * @param Closure(string): string $callback
+     */
+    protected function translate(array|string $variants, Closure $callback): string {
+        $variants   = array_values((array) $variants);
+        $translated = array_splice($variants, -1);
+        $translated = (string) end($translated);
+
+        if ($variants) {
+            foreach ($variants as $variant) {
+                $result = $callback($variant);
+
+                if ($result !== $this->key($variant)) {
+                    $translated = $result;
+                }
+            }
+        } else {
+            $translated = $callback($translated);
+        }
+
+        return $translated;
     }
 }
---
+++ packages/core/src/TranslatorTest.php
@@ -0,0 +1,201 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Core;
+
+use Closure;
+use Illuminate\Contracts\Translation\Translator;
+use LastDragon_ru\LaraASP\Core\Testing\Package\TestCase;
+use LastDragon_ru\LaraASP\Core\Translator as PackageTranslator;
+
+/**
+ * @internal
+ * @coversDefaultClass \LastDragon_ru\LaraASP\Core\Translator
+ */
+class TranslatorTest extends TestCase {
+    // <editor-fold desc="Tests">
+    // =========================================================================
+    /**
+     * @covers ::choice
+     * @covers ::translate
+     *
+     * @param array<string>|string $key
+     * @param array<mixed>         $replace
+     *
+     * @dataProvider dataProviderChoice
+     */
+    public function testChoice(
+        string $expected,
+        ?Closure $translations,
+        array|string $key,
+        int $number,
+        array $replace,
+        ?string $locale,
+    ): void {
+        $this->setTranslations($translations);
+
+        $implementation = $this->app->make(Translator::class);
+        $translator     = new class($implementation, Package::Name, null) extends PackageTranslator {
+            // empty
+        };
+
+        $this->assertEquals($expected, $translator->choice($key, $number, $replace, $locale));
+    }
+
+    /**
+     * @covers ::get
+     * @covers ::translate
+     *
+     * @param array<string>|string $key
+     * @param array<mixed>         $replace
+     *
+     * @dataProvider dataProviderGet
+     */
+    public function testGet(
+        string $expected,
+        ?Closure $translations,
+        array|string $key,
+        array $replace,
+        ?string $locale,
+    ): void {
+        $this->setTranslations($translations);
+
+        $implementation = $this->app->make(Translator::class);
+        $translator     = new class($implementation, Package::Name, null) extends PackageTranslator {
+            // empty
+        };
+
+        $this->assertEquals($expected, $translator->get($key, $replace, $locale));
+    }
+    //</editor-fold>
+
+    // <editor-fold desc="DataProviders">
+    // =========================================================================
+    /**
+     * @return array<string,array{string, ?Closure, array<string>|string, array<string,string>, ?string}>
+     */
+    public function dataProviderGet(): array {
+        return [
+            'no translation'                     => [
+                'default',
+                null,
+                ['should.not.be.translated', 'default'],
+                [
+                    'value' => 'text',
+                ],
+                null,
+            ],
+            'translation exists (no default)'    => [
+                'translated text',
+                static function (TestCase $test, string $currentLocale, string $fallbackLocale): array {
+                    return [
+                        $currentLocale => [
+                            Package::Name.'::should.be.translated' => 'translated :value',
+                        ],
+                    ];
+                },
+                'should.be.translated',
+                [
+                    'value' => 'text',
+                ],
+                null,
+            ],
+            'translation exists'                 => [
+                'translated',
+                static function (TestCase $test, string $currentLocale, string $fallbackLocale): array {
+                    return [
+                        $currentLocale => [
+                            Package::Name.'::should.be.translated' => 'translated',
+                        ],
+                    ];
+                },
+                ['should.not.be.translated', 'should.be.translated', 'default'],
+                [
+                    'value' => 'text',
+                ],
+                null,
+            ],
+            'translation exists (custom locale)' => [
+                'translated text',
+                static function (TestCase $test, string $currentLocale, string $fallbackLocale): array {
+                    return [
+                        'unk' => [
+                            Package::Name.'::should.be.translated' => 'translated :value',
+                        ],
+                    ];
+                },
+                'should.be.translated',
+                [
+                    'value' => 'text',
+                ],
+                'unk',
+            ],
+        ];
+    }
+
+    /**
+     * @return array<string,array{string, ?Closure, array<string>|string, int, array<string,string>, ?string}>
+     */
+    public function dataProviderChoice(): array {
+        return [
+            'no translation'                     => [
+                'default',
+                null,
+                ['should.not.be.translated', 'default'],
+                2,
+                [
+                    'value' => 'text',
+                ],
+                null,
+            ],
+            'translation exists (no default)'    => [
+                'translated text',
+                static function (TestCase $test, string $currentLocale, string $fallbackLocale): array {
+                    return [
+                        $currentLocale => [
+                            Package::Name.'::should.be.translated' => '{1} one |[2,*] translated :value',
+                        ],
+                    ];
+                },
+                'should.be.translated',
+                2,
+                [
+                    'value' => 'text',
+                ],
+                null,
+            ],
+            'translation exists'                 => [
+                'translated text',
+                static function (TestCase $test, string $currentLocale, string $fallbackLocale): array {
+                    return [
+                        $currentLocale => [
+                            Package::Name.'::should.be.translated' => '{1} one |[2,*] translated :value',
+                        ],
+                    ];
+                },
+                ['should.not.be.translated', 'should.be.translated', 'default'],
+                2,
+                [
+                    'value' => 'text',
+                ],
+                null,
+            ],
+            'translation exists (custom locale)' => [
+                'translated text',
+                static function (TestCase $test, string $currentLocale, string $fallbackLocale): array {
+                    return [
+                        'unk' => [
+                            Package::Name.'::should.be.translated' => '{1} one |[2,*] translated :value',
+                        ],
+                    ];
+                },
+                'should.be.translated',
+                2,
+                [
+                    'value' => 'text',
+                ],
+                'unk',
+            ],
+        ];
+    }
+    // </editor-fold>
+}
