3c512692cf27db85b118f1623af5545be30cd5bd
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-08-29 09:58:53 +0400

    fix(testing): `JsonSchemaValue` will not evaluate schema in constructor (regression).

--- packages/testing/src/Constraints/Json/JsonSchemaValue.php
+++ packages/testing/src/Constraints/Json/JsonSchemaValue.php
@@ -8,22 +8,22 @@ use LastDragon_ru\LaraASP\Testing\Utils\Args;
 use stdClass;
 
 class JsonSchemaValue implements JsonSchema {
-    protected stdClass $schema;
-
     /**
      * @param JsonSerializable|stdClass|array<mixed>|string $json
      */
-    public function __construct(JsonSerializable|stdClass|array|string $json) {
-        $schema = Args::getJson($json);
-
-        if (!($schema instanceof stdClass)) {
-            throw new InvalidArgumentJsonSchema('$schema', $json);
-        }
-
-        $this->schema = $schema;
+    public function __construct(
+        protected JsonSerializable|stdClass|array|string $json,
+    ) {
+        // empty
     }
 
     public function getSchema(): stdClass {
-        return $this->schema;
+        $schema = Args::getJson($this->json);
+
+        if (!($schema instanceof stdClass)) {
+            throw new InvalidArgumentJsonSchema('$this->json', $this->json);
+        }
+
+        return $schema;
     }
 }
