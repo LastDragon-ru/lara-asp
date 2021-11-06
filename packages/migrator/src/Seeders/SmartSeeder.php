d35a1bc7f24203fcd882895aaeaee2a4d6f963a5
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-11-06 13:35:19 +0400

    "Property Illuminate\Database\Seeder::$command (Illuminate\Console\Command) in isset() is not nullable." fix.

--- packages/migrator/src/Seeders/SmartSeeder.php
+++ packages/migrator/src/Seeders/SmartSeeder.php
@@ -75,7 +75,7 @@ abstract class SmartSeeder extends Seeder {
      * Output "skipped" message.
      */
     protected function skipped(string $reason = null): void {
-        if (isset($this->command)) {
+        if ($this->command !== null) {
             $this->command->getOutput()
                 ->writeln('<comment>         skipped</comment>'.($reason ? " ({$reason})" : ''));
         }
