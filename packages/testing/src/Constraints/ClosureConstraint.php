6220604bf4a9ae698ac80ad8c9bdf4e699aebd7f
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-11-06 09:28:26 +0400

    [testing] Better phpstan types for `ClosureConstraint`.

--- packages/testing/src/Constraints/ClosureConstraint.php
+++ packages/testing/src/Constraints/ClosureConstraint.php
@@ -5,13 +5,25 @@ namespace LastDragon_ru\LaraASP\Testing\Constraints;
 use Closure;
 use PHPUnit\Framework\Constraint\Constraint;
 
+/**
+ * @phpstan-type TClosure Closure(mixed): bool
+ */
 class ClosureConstraint extends Constraint {
+    /**
+     * @var TClosure
+     */
     private Closure $closure;
 
+    /**
+     * @param TClosure $closure
+     */
     public function __construct(Closure $closure) {
         $this->closure = $closure;
     }
 
+    /**
+     * @return TClosure
+     */
     protected function getClosure(): Closure {
         return $this->closure;
     }
