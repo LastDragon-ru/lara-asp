8067e1141d62158af3de4217a42ca2c5cfd48186
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-01-29 11:53:40 +0400

    `Printer` tests (but expected schema is invalid).

---
+++ packages/graphql/src/SchemaPrinter/PrinterTest.php
@@ -0,0 +1,138 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\GraphQL\SchemaPrinter;
+
+use Exception;
+use GraphQL\Language\Parser;
+use GraphQL\Type\Definition\CustomScalarType;
+use GraphQL\Type\Definition\EnumType;
+use GraphQL\Type\Definition\InputObjectType;
+use GraphQL\Type\Definition\InterfaceType;
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Definition\UnionType;
+use GraphQL\Type\Schema;
+use LastDragon_ru\LaraASP\GraphQL\SchemaPrinter\Settings\DefaultSettings;
+use LastDragon_ru\LaraASP\GraphQL\Testing\Package\SchemaPrinter\TestSettings;
+use LastDragon_ru\LaraASP\GraphQL\Testing\Package\TestCase;
+use Nuwave\Lighthouse\Schema\DirectiveLocator;
+use Nuwave\Lighthouse\Schema\Directives\BaseDirective;
+use Nuwave\Lighthouse\Schema\TypeRegistry;
+
+/**
+ * @internal
+ * @coversDefaultClass \LastDragon_ru\LaraASP\GraphQL\SchemaPrinter\Printer
+ */
+class PrinterTest extends TestCase {
+    // <editor-fold desc="Tests">
+    // =========================================================================
+    /**
+     * @covers ::print
+     *
+     * @dataProvider dataProviderPrint
+     */
+    public function testPrint(string $expected, Settings $settings, int $level): void {
+        // Types
+        $directives = $this->app->make(DirectiveLocator::class);
+        $registry   = $this->app->make(TypeRegistry::class);
+        $directive  = (new class() extends BaseDirective {
+            public static function definition(): string {
+                throw new Exception("Should not be called.");
+            }
+        })::class;
+
+        $codeScalar    = new CustomScalarType([
+            'name' => 'CodeScalar',
+        ]);
+        $codeEnum      = new EnumType([
+            'name'   => 'CodeEnum',
+            'values' => ['C', 'B', 'A'],
+        ]);
+        $codeInterface = new InterfaceType([
+            'name'        => 'CodeInterface',
+            'astNode'     => Parser::interfaceTypeDefinition('interface CodeInterface @codeDirective'),
+            'description' => 'Description',
+            'fields'      => [
+                [
+                    'name' => 'a',
+                    'type' => Type::boolean(),
+                ],
+            ],
+        ]);
+        $codeType      = new ObjectType([
+            'name'        => 'CodeType',
+            'astNode'     => Parser::objectTypeDefinition('type CodeType @schemaDirective'),
+            'description' => 'Description',
+            'fields'      => [
+                [
+                    'name' => 'a',
+                    'type' => Type::boolean(),
+                ],
+            ],
+        ]);
+        $codeUnion     = new UnionType([
+            'name'  => 'CodeUnion',
+            'types' => [
+                $codeType,
+            ],
+        ]);
+        $codeInput     = new InputObjectType([
+            'name'        => 'CodeInput',
+            'astNode'     => Parser::inputObjectTypeDefinition('input InputObjectType @schemaDirective'),
+            'description' => 'Description',
+            'fields'      => [
+                [
+                    'name' => 'a',
+                    'type' => Type::boolean(),
+                ],
+            ],
+        ]);
+
+        $directives->setResolved('schemaDirective', $directive);
+        $directives->setResolved('schemaDirectiveUnused', $directive);
+        $directives->setResolved(
+            'codeDirective',
+            (new class() extends BaseDirective {
+                public static function definition(): string {
+                    return "directive @codeDirective repeatable on SCHEMA | SCALAR | INTERFACE";
+                }
+            })::class
+        );
+        $registry->register($codeScalar);
+        $registry->register($codeEnum);
+        $registry->register($codeInterface);
+        $registry->register($codeType);
+        $registry->register($codeUnion);
+        $registry->register($codeInput);
+
+        // Test
+        $expected = $this->getTestData()->content($expected);
+        $schema  = $this->getGraphQLSchema($this->getTestData()->file('~schema.graphql'));
+        $printer = (new Printer())->setSettings($settings)->setLevel($level);
+        $actual  = $printer->print($schema);
+
+        self::assertEquals($expected, (string) $actual);
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="DataProviders">
+    // =========================================================================
+    /**
+     * @return array<string, array{string, Settings, int}>
+     */
+    public function dataProviderPrint(): array {
+        return [
+            DefaultSettings::class => [
+                '~default-settings.graphql',
+                new DefaultSettings(),
+                0,
+            ],
+            TestSettings::class => [
+                '~test-settings.graphql',
+                new TestSettings(),
+                0,
+            ],
+        ];
+    }
+    // </editor-fold>
+}
