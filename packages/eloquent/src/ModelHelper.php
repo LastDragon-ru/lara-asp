a63f2720a5151d047015ca9d6da210dfe1befecf
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2022-02-05 09:43:57 +0400

    feat(eloquent): Result of `ModelHelper::isRelation()` will be cached (cache can be reset by `ModelHelper::resetRelationsCache()`)

--- packages/eloquent/src/ModelHelper.php
+++ packages/eloquent/src/ModelHelper.php
@@ -14,6 +14,11 @@ use function is_a;
 use function is_string;
 
 class ModelHelper {
+    /**
+     * @var array<class-string<Model>, array<string, bool>>
+     */
+    private static array $relations = [];
+
     private bool  $builder;
     private Model $model;
 
@@ -40,38 +45,46 @@ class ModelHelper {
     }
 
     public function isRelation(string $name): bool {
-        try {
-            return (bool) $this->getRelation($name);
-        } catch (PropertyIsNotRelation) {
-            return false;
+        $model = $this->getModel()::class;
+
+        if (!isset(self::$relations[$model][$name])) {
+            self::$relations[$model][$name] = false;
+
+            try {
+                $class                         = new ReflectionClass($model);
+                $type                          = $class->getMethod($name)->getReturnType();
+                self::$relations[$model][$name] = $type instanceof ReflectionNamedType
+                    && is_a($type->getName(), Relation::class, true);
+            } catch (ReflectionException) {
+                // empty
+            }
         }
+
+        return self::$relations[$model][$name];
     }
 
     public function getRelation(string $name): Relation {
         $relation = null;
+        $model    = $this->getModel();
 
-        try {
-            $model = $this->getModel();
-            $class = new ReflectionClass($model);
-            $type  = $class->getMethod($name)->getReturnType();
-
-            if ($type instanceof ReflectionNamedType && is_a($type->getName(), Relation::class, true)) {
-                if ($this->isBuilder()) {
-                    $relation = Relation::noConstraints(static function () use ($model, $name) {
-                        return $model->newModelInstance()->{$name}();
-                    });
-                } else {
-                    $relation = $model->{$name}();
-                }
+        if ($this->isRelation($name)) {
+            if ($this->isBuilder()) {
+                $relation = Relation::noConstraints(static function () use ($model, $name) {
+                    return $model->newModelInstance()->{$name}();
+                });
+            } else {
+                $relation = $model->{$name}();
             }
-        } catch (ReflectionException) {
-            $relation = null;
         }
 
         if (!($relation instanceof Relation)) {
-            throw new PropertyIsNotRelation($this->getModel(), $name);
+            throw new PropertyIsNotRelation($model, $name);
         }
 
         return $relation;
+    }
+
+    public static function resetRelationsCache(): void {
+        self::$relations = [];
     }
 }
