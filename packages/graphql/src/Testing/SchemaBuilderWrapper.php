d281b7e74f8b76130a3f1173590d542ac91906b6
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2023-03-23 11:21:35 +0400

    `SchemaBuilderWrapper` will not use temp directory for cache.

--- packages/graphql/src/Testing/SchemaBuilderWrapper.php
+++ packages/graphql/src/Testing/SchemaBuilderWrapper.php
@@ -3,12 +3,8 @@
 namespace LastDragon_ru\LaraASP\GraphQL\Testing;
 
 use GraphQL\Type\Schema;
-use Illuminate\Config\Repository;
 use Illuminate\Container\Container;
-use Illuminate\Contracts\Config\Repository as ConfigContract;
 use Illuminate\Contracts\Events\Dispatcher as EventsDispatcher;
-use Illuminate\Support\Arr;
-use LastDragon_ru\LaraASP\Testing\Utils\WithTempDirectory;
 use Nuwave\Lighthouse\Schema\AST\ASTBuilder;
 use Nuwave\Lighthouse\Schema\AST\ASTCache;
 use Nuwave\Lighthouse\Schema\DirectiveLocator;
@@ -19,16 +15,12 @@ use Nuwave\Lighthouse\Schema\TypeRegistry;
 use function spl_object_hash;
 
 class SchemaBuilderWrapper extends SchemaBuilder {
-    use WithTempDirectory;
-
     protected ?SchemaBuilder $current = null;
 
     /**
      * @var array<class-string, object>
      */
     protected array $singletons = [];
-
-    private string $directory;
 
     /**
      * @noinspection             PhpMissingParentConstructorInspection
@@ -38,10 +30,6 @@ class SchemaBuilderWrapper extends SchemaBuilder {
         protected SchemaBuilder $builder,
     ) {
         // no need to call parent
-    }
-
-    protected function getDirectory(): string {
-        return $this->directory ??= static::getTempDirectory();
     }
 
     protected function getSchemaBuilder(): SchemaBuilder {
@@ -67,16 +55,10 @@ class SchemaBuilderWrapper extends SchemaBuilder {
         $builder = null;
 
         if ($provider) {
-            $config = $container->make(ConfigContract::class)->all();
-            $path   = $this->getDirectory().'/'.spl_object_hash($provider).'.php';
-
-            Arr::set($config, 'lighthouse.schema_cache.enable', true);
-            Arr::set($config, 'lighthouse.schema_cache.path', $path);
-
             $types      = $container->make(TypeRegistry::class);
             $dispatcher = $container->make(EventsDispatcher::class);
             $directives = $container->make(DirectiveLocator::class);
-            $astCache   = new ASTCache(new Repository($config));
+            $astCache   = $container->make(SchemaCache::class, ['key' => spl_object_hash($provider)]);
             $astBuilder = new ASTBuilder($directives, $provider, $dispatcher, $astCache);
             $builder    = new SchemaBuilder($types, $astBuilder);
 
