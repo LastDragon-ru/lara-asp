bb10d1c189b36cad4ef7cc83364b978ce63f1845
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2023-03-30 10:48:51 +0400

    Added `OperatorDirective::getDirectiveLocations()`.

--- packages/graphql/src/Builder/Directives/OperatorDirective.php
+++ packages/graphql/src/Builder/Directives/OperatorDirective.php
@@ -3,8 +3,11 @@
 namespace LastDragon_ru\LaraASP\GraphQL\Builder\Directives;
 
 use GraphQL\Language\AST\DirectiveNode;
+use GraphQL\Language\DirectiveLocation;
 use LastDragon_ru\LaraASP\GraphQL\Builder\Contracts\Operator;
 use Nuwave\Lighthouse\Schema\Directives\BaseDirective;
+
+use function implode;
 
 abstract class OperatorDirective extends BaseDirective implements Operator {
     public function __construct() {
@@ -12,11 +15,21 @@ abstract class OperatorDirective extends BaseDirective implements Operator {
     }
 
     public static function definition(): string {
-        $name = static::getDirectiveName();
+        $name      = static::getDirectiveName();
+        $locations = implode('|', static::getDirectiveLocations());
 
         return /** @lang GraphQL */ <<<GRAPHQL
-            directive {$name} on INPUT_FIELD_DEFINITION
+            directive {$name} on {$locations}
         GRAPHQL;
+    }
+
+    /**
+     * @return non-empty-list<string>
+     */
+    protected static function getDirectiveLocations(): array {
+        return [
+            DirectiveLocation::INPUT_FIELD_DEFINITION,
+        ];
     }
 
     public function getFieldDirective(): ?DirectiveNode {
