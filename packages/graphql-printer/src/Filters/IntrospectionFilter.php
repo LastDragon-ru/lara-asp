0c6dac7f639a5ad12fb911e1372c7dacbb67bd31
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2023-04-14 08:42:49 +0400

    `IntrospectionFilter` marked as `@internal`.

--- packages/graphql-printer/src/Filters/IntrospectionFilter.php
+++ packages/graphql-printer/src/Filters/IntrospectionFilter.php
@@ -5,6 +5,9 @@ namespace LastDragon_ru\LaraASP\GraphQLPrinter\Filters;
 use LastDragon_ru\LaraASP\GraphQLPrinter\Contracts\DirectiveFilter;
 use LastDragon_ru\LaraASP\GraphQLPrinter\Contracts\TypeFilter;
 
+/**
+ * @internal
+ */
 class IntrospectionFilter implements TypeFilter, DirectiveFilter {
     public function isAllowedDirective(string $directive, bool $isStandard): bool {
         return true;
