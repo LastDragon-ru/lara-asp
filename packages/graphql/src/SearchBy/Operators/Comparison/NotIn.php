cd24d54a5b213026b7981326f7f748c7d31dc98e
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2023-09-08 08:57:28 +0400

    feat(graphql/@searchBy): Scout support for `NotIn` operator (`laravel/scout:>=10.3.0` required).
    (cherry picked from commit 53087fd6a4c37674132cee9785f3a9fb32781971)

--- packages/graphql/src/SearchBy/Operators/Comparison/NotIn.php
+++ packages/graphql/src/SearchBy/Operators/Comparison/NotIn.php
@@ -4,15 +4,19 @@ namespace LastDragon_ru\LaraASP\GraphQL\SearchBy\Operators\Comparison;
 
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Query\Builder as QueryBuilder;
+use Laravel\Scout\Builder as ScoutBuilder;
 use LastDragon_ru\LaraASP\GraphQL\Builder\Contracts\Handler;
 use LastDragon_ru\LaraASP\GraphQL\Builder\Contracts\TypeProvider;
 use LastDragon_ru\LaraASP\GraphQL\Builder\Contracts\TypeSource;
 use LastDragon_ru\LaraASP\GraphQL\Builder\Exceptions\OperatorUnsupportedBuilder;
 use LastDragon_ru\LaraASP\GraphQL\Builder\Property;
 use LastDragon_ru\LaraASP\GraphQL\SearchBy\Operators\BaseOperator;
+use LastDragon_ru\LaraASP\GraphQL\SearchBy\Operators\Traits\ScoutSupport;
 use Nuwave\Lighthouse\Execution\Arguments\Argument;
 
 class NotIn extends BaseOperator {
+    use ScoutSupport;
+
     public static function getName(): string {
         return 'notIn';
     }
@@ -25,15 +29,23 @@ class NotIn extends BaseOperator {
         return "[{$source->getTypeName()}!]";
     }
 
+    protected function getScoutVersion(): ?string {
+        return '>=10.3.0';
+    }
+
     public function call(Handler $handler, object $builder, Property $property, Argument $argument): object {
-        if (!($builder instanceof EloquentBuilder || $builder instanceof QueryBuilder)) {
+        $property = $property->getParent();
+        $value    = (array) $argument->toPlain();
+
+        if ($builder instanceof EloquentBuilder || $builder instanceof QueryBuilder) {
+            $builder->whereNotIn((string) $property, $value);
+        } elseif ($builder instanceof ScoutBuilder) {
+            $property = $this->getFieldResolver()->getField($builder->model, $property);
+
+            $builder->whereNotIn($property, $value);
+        } else {
             throw new OperatorUnsupportedBuilder($this, $builder);
         }
-
-        $property = (string) $property->getParent();
-        $value    = $argument->toPlain();
-
-        $builder->whereNotIn($property, $value);
 
         return $builder;
     }
--- packages/graphql/src/SearchBy/Operators/Comparison/NotInTest.php
+++ packages/graphql/src/SearchBy/Operators/Comparison/NotInTest.php
@@ -3,15 +3,23 @@
 namespace LastDragon_ru\LaraASP\GraphQL\SearchBy\Operators\Comparison;
 
 use Closure;
+use Composer\InstalledVersions;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Builder as ScoutBuilder;
 use LastDragon_ru\LaraASP\GraphQL\Builder\Contracts\Handler;
+use LastDragon_ru\LaraASP\GraphQL\Builder\Contracts\Scout\FieldResolver;
 use LastDragon_ru\LaraASP\GraphQL\Builder\Property;
 use LastDragon_ru\LaraASP\GraphQL\Testing\Package\DataProviders\BuilderDataProvider;
+use LastDragon_ru\LaraASP\GraphQL\Testing\Package\DataProviders\ScoutBuilderDataProvider;
 use LastDragon_ru\LaraASP\GraphQL\Testing\Package\TestCase;
 use LastDragon_ru\LaraASP\Testing\Providers\ArrayDataProvider;
 use LastDragon_ru\LaraASP\Testing\Providers\CompositeDataProvider;
 use Mockery;
 use Nuwave\Lighthouse\Execution\Arguments\Argument;
 use PHPUnit\Framework\Attributes\CoversClass;
+
+use function implode;
+use function sprintf;
 
 /**
  * @internal
@@ -43,6 +51,47 @@ class NotInTest extends TestCase {
         $builder  = $operator->call($search, $builder, $property, $argument);
 
         self::assertDatabaseQueryEquals($expected, $builder);
+    }
+
+    /**
+     * @dataProvider dataProviderCallScout
+     *
+     * @param array<string, mixed>          $expected
+     * @param Closure(static): ScoutBuilder $builderFactory
+     * @param Closure(static): Argument     $argumentFactory
+     * @param Closure():FieldResolver|null  $resolver
+     */
+    public function testCallScout(
+        array $expected,
+        Closure $builderFactory,
+        Property $property,
+        Closure $argumentFactory,
+        Closure $resolver = null,
+    ): void {
+        // Prepare
+        if ($resolver) {
+            $this->override(FieldResolver::class, $resolver);
+        }
+
+        // Supported?
+        $operator = $this->app->make(NotInTest_Operator::class);
+
+        if (!$operator->isScoutSupported()) {
+            self::markTestSkipped(sprintf(
+                'Minimum version of `laravel/scout` should be `%s`, `%s` installed.',
+                $operator->getScoutVersion(),
+                InstalledVersions::getPrettyVersion('laravel/scout'),
+            ));
+        }
+
+        // Test
+        $property = $property->getChild('operator name should be ignored');
+        $argument = $argumentFactory($this);
+        $search   = Mockery::mock(Handler::class);
+        $builder  = $builderFactory($this);
+        $builder  = $operator->call($search, $builder, $property, $argument);
+
+        self::assertScoutQueryEquals($expected, $builder);
     }
     // </editor-fold>
 
@@ -78,5 +127,66 @@ class NotInTest extends TestCase {
             ]),
         ))->getData();
     }
+
+    /**
+     * @return array<array-key, mixed>
+     */
+    public static function dataProviderCallScout(): array {
+        return (new CompositeDataProvider(
+            new ScoutBuilderDataProvider(),
+            new ArrayDataProvider([
+                'property'               => [
+                    [
+                        'whereNotIns' => [
+                            'path.to.property' => [1, 2, 3],
+                        ],
+                    ],
+                    new Property('path', 'to', 'property'),
+                    static function (self $test): Argument {
+                        return $test->getGraphQLArgument('[Int!]!', [1, 2, 3]);
+                    },
+                    null,
+                ],
+                'property with resolver' => [
+                    [
+                        'whereNotIns' => [
+                            'properties/path/to/property' => [1, 2, 3],
+                        ],
+                    ],
+                    new Property('path', 'to', 'property'),
+                    static function (self $test): Argument {
+                        return $test->getGraphQLArgument('[Int!]!', [1, 2, 3]);
+                    },
+                    static function (): FieldResolver {
+                        return new class() implements FieldResolver {
+                            /**
+                             * @inheritDoc
+                             */
+                            public function getField(Model $model, Property $property): string {
+                                return 'properties/'.implode('/', $property->getPath());
+                            }
+                        };
+                    },
+                ],
+            ]),
+        ))->getData();
+    }
     // </editor-fold>
+}
+
+// @phpcs:disable PSR1.Classes.ClassDeclaration.MultipleClasses
+// @phpcs:disable Squiz.Classes.ValidClassName.NotCamelCaps
+
+/**
+ * @internal
+ * @noinspection PhpMultipleClassesDeclarationsInOneFile
+ */
+class NotInTest_Operator extends NotIn {
+    public function isScoutSupported(): bool {
+        return parent::isScoutSupported();
+    }
+
+    public function getScoutVersion(): ?string {
+        return parent::getScoutVersion();
+    }
 }
