a5076ffca8fb873fa4195278f3eb7daa102b91c2
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2023-10-24 09:20:58 +0400

    deprecate(documentator): `LastDragon_ru\LaraASP\Documentator\Preprocessor\Instruction` (please use `LastDragon_ru\LaraASP\Documentator\Preprocessor\Contracts\Instruction` and its subclasses instead).

--- packages/documentator/src/Preprocessor/Instruction.php
+++ packages/documentator/src/Preprocessor/Instruction.php
@@ -2,14 +2,14 @@
 
 namespace LastDragon_ru\LaraASP\Documentator\Preprocessor;
 
+use LastDragon_ru\LaraASP\Documentator\Preprocessor\Contracts\Instruction as InstructionContract;
 use LastDragon_ru\LaraASP\Documentator\Preprocessor\Contracts\ProcessableInstruction;
 
+/**
+ * @deprecated 5.0.0 Please use {@see InstructionContract} and its subclasses instead.
+ *
+ * @see InstructionContract
+ */
 interface Instruction extends ProcessableInstruction {
-    public static function getName(): string;
-
-    public static function getDescription(): string;
-
-    public static function getTargetDescription(): ?string;
-
-    public function process(string $path, string $target): string;
+    // empty
 }
