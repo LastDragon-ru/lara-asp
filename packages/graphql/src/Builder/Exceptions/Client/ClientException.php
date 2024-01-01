24cb66f3f9e5dae33b6bd3fae11bad617c6a101b
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2024-01-01 14:33:13 +0400

    refactor(graphql)!: Removed deprecated `ClientException::getCategory()`.

--- packages/graphql/src/Builder/Exceptions/Client/ClientException.php
+++ packages/graphql/src/Builder/Exceptions/Client/ClientException.php
@@ -4,19 +4,11 @@ namespace LastDragon_ru\LaraASP\GraphQL\Builder\Exceptions\Client;
 
 use GraphQL\Error\ClientAware;
 use LastDragon_ru\LaraASP\GraphQL\Builder\Exceptions\BuilderException;
-use LastDragon_ru\LaraASP\GraphQL\Package;
 use Override;
 
 abstract class ClientException extends BuilderException implements ClientAware {
     #[Override]
     public function isClientSafe(): bool {
         return true;
-    }
-
-    /**
-     * @deprecated 5.2.0 Not needed anymore and will be removed soon.
-     */
-    public function getCategory(): string {
-        return Package::Name;
     }
 }
