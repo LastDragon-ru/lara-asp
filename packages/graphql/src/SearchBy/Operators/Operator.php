b87dd7dd301be86669f622854dcd2638c3df6c81
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2024-02-28 08:51:43 +0400

    `TypeExtender` will not be used (another way found)

--- packages/graphql/src/SearchBy/Operators/Operator.php
+++ packages/graphql/src/SearchBy/Operators/Operator.php
@@ -8,18 +8,12 @@ use LastDragon_ru\LaraASP\GraphQL\Builder\Contracts\Context;
 use LastDragon_ru\LaraASP\GraphQL\Builder\Contracts\TypeProvider;
 use LastDragon_ru\LaraASP\GraphQL\Builder\Contracts\TypeSource;
 use LastDragon_ru\LaraASP\GraphQL\Builder\Directives\OperatorDirective;
-use LastDragon_ru\LaraASP\GraphQL\Builder\Traits\TypeExtender;
 use LastDragon_ru\LaraASP\GraphQL\SearchBy\Contracts\Operator as Marker;
-use LastDragon_ru\LaraASP\GraphQL\SearchBy\Operators;
-use LastDragon_ru\LaraASP\GraphQL\Utils\AstManipulator;
-use Nuwave\Lighthouse\Support\Contracts\TypeExtensionManipulator;
 use Override;
 
 use function is_a;
 
-abstract class Operator extends OperatorDirective implements Marker, TypeExtensionManipulator {
-    use TypeExtender;
-
+abstract class Operator extends OperatorDirective implements Marker {
     #[Override]
     public function getFieldType(TypeProvider $provider, TypeSource $source, Context $context): ?string {
         return $source->getTypeName();
@@ -29,13 +23,5 @@ abstract class Operator extends OperatorDirective implements Marker, TypeExtensi
     protected function isBuilderSupported(string $builder): bool {
         return is_a($builder, EloquentBuilder::class, true)
             || is_a($builder, QueryBuilder::class, true);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    #[Override]
-    protected function getExtendableScalars(AstManipulator $manipulator): array {
-        return Operators::getExtendableScalars($manipulator);
     }
 }
--- packages/graphql/src/SortBy/Operators/Operator.php
+++ packages/graphql/src/SortBy/Operators/Operator.php
@@ -5,29 +5,15 @@ namespace LastDragon_ru\LaraASP\GraphQL\SortBy\Operators;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 use LastDragon_ru\LaraASP\GraphQL\Builder\Directives\OperatorDirective;
-use LastDragon_ru\LaraASP\GraphQL\Builder\Traits\TypeExtender;
 use LastDragon_ru\LaraASP\GraphQL\SortBy\Contracts\Operator as Marker;
-use LastDragon_ru\LaraASP\GraphQL\SortBy\Operators;
-use LastDragon_ru\LaraASP\GraphQL\Utils\AstManipulator;
-use Nuwave\Lighthouse\Support\Contracts\TypeExtensionManipulator;
 use Override;
 
 use function is_a;
 
-abstract class Operator extends OperatorDirective implements Marker, TypeExtensionManipulator {
-    use TypeExtender;
-
+abstract class Operator extends OperatorDirective implements Marker {
     #[Override]
     protected function isBuilderSupported(string $builder): bool {
         return is_a($builder, EloquentBuilder::class, true)
             || is_a($builder, QueryBuilder::class, true);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    #[Override]
-    protected function getExtendableScalars(AstManipulator $manipulator): array {
-        return Operators::getExtendableScalars($manipulator);
     }
 }
