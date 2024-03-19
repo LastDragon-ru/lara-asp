003fb54aa33ccaf1c05f047e02ded9c1702cbeee
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2024-03-19 08:28:41 +0400

    [migrator] No `Illuminate\Support\Collection`.

--- packages/migrator/src/Commands/RawMigrationTest.php
+++ packages/migrator/src/Commands/RawMigrationTest.php
@@ -4,13 +4,11 @@ namespace LastDragon_ru\LaraASP\Migrator\Commands;
 
 use Composer\InstalledVersions;
 use Composer\Semver\VersionParser;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Composer;
 use LastDragon_ru\LaraASP\Migrator\Package;
 use LastDragon_ru\LaraASP\Migrator\Testing\Package\TestCase;
 use Mockery;
 use PHPUnit\Framework\Attributes\CoversClass;
-use SplFileInfo;
 use Symfony\Component\Finder\Finder;
 
 use function array_slice;
@@ -61,12 +59,11 @@ final class RawMigrationTest extends TestCase {
             'raw_migration~down.sql',
             'raw_migration~up.sql',
         ];
-        $actual   = (new Collection($finder->files()->sortByName()))
-            ->map(static function (SplFileInfo $file): string {
-                return implode('_', array_slice(explode('_', $file->getFilename()), 4));
-            })
-            ->values()
-            ->all();
+        $actual   = [];
+
+        foreach ($finder->files()->sortByName() as $file) {
+            $actual[] = implode('_', array_slice(explode('_', $file->getFilename()), 4));
+        }
 
         self::assertEquals($expected, $actual);
     }
