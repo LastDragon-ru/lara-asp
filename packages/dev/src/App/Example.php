75b27037bc7498ca2824f6e1df9439696e659d5c
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2024-05-27 08:46:27 +0400

    chore(dev): Removed `dev:example` command (the `Runner` contract will be used to run examples).

--- packages/dev/src/App/Example.php
+++ packages/dev/src/App/Example.php
@@ -2,12 +2,14 @@
 
 namespace LastDragon_ru\LaraASP\Dev\App;
 
-use Illuminate\Console\Command;
 use Illuminate\Contracts\Config\Repository;
 use Illuminate\Contracts\Foundation\Application;
-use LastDragon_ru\LaraASP\Core\Utils\Cast;
+use LastDragon_ru\LaraASP\Core\Application\ApplicationResolver;
 use LastDragon_ru\LaraASP\Core\Utils\ConfigMerger;
+use LastDragon_ru\LaraASP\Documentator\Preprocessor\Instructions\IncludeExample\Contracts\Runner;
+use LastDragon_ru\LaraASP\Documentator\Processor\FileSystem\File;
 use LogicException;
+use Override;
 use PhpParser\ErrorHandler\Collecting;
 use PhpParser\Node;
 use PhpParser\Node\Expr\StaticCall;
@@ -17,36 +19,29 @@ use PhpParser\NodeFinder;
 use PhpParser\ParserFactory;
 use PhpParser\PrettyPrinter\Standard;
 use Stringable;
-use Symfony\Component\Console\Attribute\AsCommand;
 
 use function array_map;
 use function array_slice;
 use function debug_backtrace;
 use function end;
-use function file;
 use function implode;
+use function preg_split;
 use function sprintf;
+use function str_contains;
 use function trim;
 
 use const DEBUG_BACKTRACE_IGNORE_ARGS;
 
-#[AsCommand(
-    name       : Example::Name,
-    description: 'Executes example file within Application context.',
-)]
-final class Example extends Command {
-    private const Name = 'dev:example';
-
+final class Example implements Runner {
     private static ?Application $app    = null;
+    private static ?File        $file   = null;
     private static ?Dumper      $dumper = null;
 
-    /**
-     * @phpcsSuppress SlevomatCodingStandard.TypeHints.PropertyTypeHint.MissingNativeTypeHint
-     * @var string
-     */
-    protected $signature = self::Name.<<<'SIGNATURE'
-        {file : example file}
-    SIGNATURE;
+    public function __construct(
+        private readonly ApplicationResolver $appResolver,
+    ) {
+        // empty
+    }
 
     protected static function getDumper(): Dumper {
         if (!self::$dumper) {
@@ -61,28 +56,39 @@ final class Example extends Command {
         return self::$dumper;
     }
 
-    public function __invoke(Dumper $dumper): void {
-        self::$dumper = $dumper;
-        self::$app    = $this->laravel;
-        $file         = Cast::toString($this->argument('file'));
+    #[Override]
+    public function __invoke(File $file): ?string {
+        // Runnable?
+        if ($file->getExtension() !== 'php' || !str_contains($file->getContent(), 'Example::')) {
+            return null;
+        }
+
+        // Run
+        $result       = null;
+        self::$app    = $this->appResolver->getInstance();
+        self::$file   = $file;
+        self::$dumper = self::$app->make(Dumper::class);
 
         try {
-            // Run
+            // Execute
             (static function () use ($file): void {
-                include $file;
+                include $file->getPath();
             })();
 
             // Output
-            $dumps  = $dumper->getDumps();
+            $dumps  = self::$dumper->getDumps();
             $output = implode("\n\n", array_map(trim(...), $dumps));
 
             if ($output) {
-                $this->output->writeln("<markdown>{$output}</markdown>");
+                $result = "<markdown>{$output}</markdown>";
             }
         } finally {
             self::$app    = null;
+            self::$file   = null;
             self::$dumper = null;
         }
+
+        return $result;
     }
 
     public static function dump(mixed $value, string $expression = null): void {
@@ -130,7 +136,8 @@ final class Example extends Command {
         }
 
         // Extract first arg
-        $code   = implode("\n", array_slice((array) file($context['file']), $context['line'] - 1));
+        $lines  = preg_split('/\R/u', self::$file?->getContent() ?? '') ?: [];
+        $code   = implode("\n", array_slice($lines, $context['line'] - 1));
         $parser = (new ParserFactory())->createForNewestSupportedVersion();
         $stmts  = (array) $parser->parse("<?php\n{$code}", new Collecting());
         $call   = (new NodeFinder())->findFirst($stmts, static function (Node $node) use ($method): bool {
