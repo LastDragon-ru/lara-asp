450302323b1fdb5e4a1ceb7a6fc0215d40727993
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2024-08-13 09:54:48 +0400

    `FootnotesPrefix` mutation.

---
+++ packages/documentator/src/Markdown/Mutations/FootnotesPrefix.php
@@ -0,0 +1,98 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Documentator\Markdown\Mutations;
+
+use LastDragon_ru\LaraASP\Documentator\Markdown\Contracts\Mutation;
+use LastDragon_ru\LaraASP\Documentator\Markdown\Document;
+use LastDragon_ru\LaraASP\Documentator\Markdown\Location\Location;
+use LastDragon_ru\LaraASP\Documentator\Markdown\Location\Locator;
+use LastDragon_ru\LaraASP\Documentator\Markdown\Utils;
+use League\CommonMark\Extension\Footnote\Node\Footnote;
+use League\CommonMark\Extension\Footnote\Node\FootnoteRef;
+use League\CommonMark\Node\Block\Document as DocumentNode;
+use Override;
+
+use function hash;
+use function mb_strlen;
+use function mb_substr;
+use function uniqid;
+
+/**
+ * Adds unique prefix for all footnotes.
+ */
+readonly class FootnotesPrefix implements Mutation {
+    public function __construct(
+        /**
+         * If the prefix is not specified, the hash of the document path will
+         * be used. If the document path is unknown, the random hash will be
+         * used.
+         */
+        protected ?string $prefix = null,
+    ) {
+        // empty
+    }
+
+    /**
+     * @inheritDoc
+     */
+    #[Override]
+    public function __invoke(Document $document, DocumentNode $node): array {
+        $prefix  = $this->prefix ?: hash('xxh3', $document->getPath() ?: uniqid($this::class)); // @phpstan-ignore disallowed.function
+        $changes = [];
+
+        foreach ($node->iterator() as $child) {
+            // Footnote?
+            if (!($child instanceof FootnoteRef) && !($child instanceof Footnote)) {
+                continue;
+            }
+
+            // Replace
+            $label    = $this->getLabel($document, $child);
+            $location = $label ? $this->getLabelLocation($child, $label) : null;
+
+            if ($location) {
+                $changes[] = [$location, "{$prefix}-{$label}"];
+            }
+        }
+
+        return $changes;
+    }
+
+    private function getLabel(Document $document, Footnote|FootnoteRef $footnote): ?string {
+        // The thephpleague/commonmark replaces the original title of
+        // `FootnoteRef` to make it unique. We need to find original.
+        $label = $footnote->getReference()->getLabel();
+
+        if ($footnote instanceof FootnoteRef) {
+            $location = Utils::getLocation($footnote);
+            $label    = $location
+                ? (mb_substr($document->getText($location) ?? '', 2, -1) ?: '')
+                : null;
+        }
+
+        return $label;
+    }
+
+    private function getLabelLocation(Footnote|FootnoteRef $footnote, string $label): ?Location {
+        // Get the start line
+        $location   = Utils::getLocation($footnote);
+        $coordinate = null;
+
+        foreach ($location ?? [] as $c) {
+            $coordinate = $c;
+            break;
+        }
+
+        if ($coordinate === null) {
+            return null;
+        }
+
+        // Location
+        $startLine = $coordinate->line;
+        $endLine   = $startLine;
+        $offset    = $coordinate->offset + 2;
+        $length    = mb_strlen($label);
+
+        return new Locator($startLine, $endLine, $offset, $length);
+    }
+}
