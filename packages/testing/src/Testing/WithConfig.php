7aa7399a680e855fff02ad73140f8027b74dc6ab
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2024-10-22 10:59:04 +0400

    Added `\LastDragon_ru\LaraASP\Testing\Testing\WithConfig::setConfiguration()` and `\LastDragon_ru\LaraASP\Testing\Testing\WithConfig::assertConfigurationExportable()` helpers.

--- packages/testing/src/Testing/WithConfig.php
+++ packages/testing/src/Testing/WithConfig.php
@@ -3,9 +3,11 @@
 namespace LastDragon_ru\LaraASP\Testing\Testing;
 
 use Illuminate\Contracts\Config\Repository;
-use Illuminate\Contracts\Foundation\Application;
+use LastDragon_ru\LaraASP\Core\Application\Configuration\Configuration;
+use LastDragon_ru\LaraASP\Core\Application\Configuration\ConfigurationResolver;
 
 use function is_callable;
+use function var_export;
 
 /**
  * Allows replacing settings for Laravel.
@@ -17,10 +19,10 @@ use function is_callable;
  * @phpstan-type Settings         array<string,mixed>
  * @phpstan-type SettingsCallback callable(static, Repository): Settings
  * @phpstan-type SettingsFactory  SettingsCallback|Settings|null
+ *
+ * @phpstan-require-extends TestCase
  */
 trait WithConfig {
-    abstract protected function app(): Application;
-
     /**
      * @param SettingsFactory $settings
      */
@@ -31,5 +33,31 @@ trait WithConfig {
         if ($settings !== null) {
             $repository->set($settings);
         }
+    }
+
+    /**
+     * @template T of Configuration
+     *
+     * @param class-string<ConfigurationResolver<T>> $resolver
+     * @param callable(T): void|null                 $callback
+     */
+    public function setConfiguration(string $resolver, ?callable $callback): void {
+        if ($callback !== null) {
+            $callback($this->app()->make($resolver)->getInstance());
+        }
+    }
+
+    /**
+     * @template T of Configuration
+     *
+     * @param class-string<ConfigurationResolver<T>> $resolver
+     */
+    public function assertConfigurationExportable(string $resolver): void {
+        $config   = $resolver::getDefaultConfig();
+        $exported = var_export($config, true);
+        $exported = "<?php declare(strict_types = 1);\n\nreturn {$exported};";
+        $imported = require static::getTempFile($exported)->getPathname();
+
+        self::assertEquals($config, $imported);
     }
 }
