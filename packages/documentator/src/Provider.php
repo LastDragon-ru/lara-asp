a9518b2879e7a6cf4ea6a71865b3d3c772bbb296
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2024-11-16 10:43:08 +0400

    `\LastDragon_ru\LaraASP\Documentator\Provider` => `\LastDragon_ru\LaraASP\Documentator\PackageProvider`.

--- packages/documentator/src/Provider.php
+++ packages/documentator/src/Provider.php
@@ -2,39 +2,9 @@
 
 namespace LastDragon_ru\LaraASP\Documentator;
 
-use Illuminate\Support\ServiceProvider;
-use LastDragon_ru\LaraASP\Core\Provider\WithViews;
-use LastDragon_ru\LaraASP\Documentator\Commands\Commands;
-use LastDragon_ru\LaraASP\Documentator\Commands\Preprocess;
-use LastDragon_ru\LaraASP\Documentator\Commands\Requirements;
-use LastDragon_ru\LaraASP\Documentator\Processor\Contracts\Factory as ProcessorFactoryContract;
-use LastDragon_ru\LaraASP\Documentator\Processor\Factory as ProcessorFactory;
-use LastDragon_ru\LaraASP\Documentator\Processor\Tasks\CodeLinks\Contracts\LinkFactory as LinkFactoryContract;
-use LastDragon_ru\LaraASP\Documentator\Processor\Tasks\CodeLinks\Links\Factory as LinkFactory;
-use Override;
-
-class Provider extends ServiceProvider {
-    use WithViews;
-
-    #[Override]
-    public function register(): void {
-        parent::register();
-
-        $this->app->scopedIf(ProcessorFactoryContract::class, ProcessorFactory::class);
-        $this->app->scopedIf(LinkFactoryContract::class, LinkFactory::class);
-    }
-
-    public function boot(): void {
-        $this->bootViews();
-        $this->commands(
-            Requirements::class,
-            Preprocess::class,
-            Commands::class,
-        );
-    }
-
-    #[Override]
-    protected function getName(): string {
-        return Package::Name;
-    }
+/**
+ * @deprecated %{VERSION} The {@see PackageProvider} should be used instead.
+ */
+class Provider extends PackageProvider {
+    // empty
 }
--- packages/documentator/src/Testing/Package/TestCase.php
+++ packages/documentator/src/Testing/Package/TestCase.php
@@ -3,7 +3,7 @@
 namespace LastDragon_ru\LaraASP\Documentator\Testing\Package;
 
 use LastDragon_ru\LaraASP\Core\PackageProvider as CoreProvider;
-use LastDragon_ru\LaraASP\Documentator\Provider;
+use LastDragon_ru\LaraASP\Documentator\PackageProvider;
 use LastDragon_ru\LaraASP\Serializer\Provider as SerializerProvider;
 use LastDragon_ru\LaraASP\Testing\Testing\TestCase as PackageTestCase;
 use Override;
@@ -20,7 +20,7 @@ abstract class TestCase extends PackageTestCase {
     #[Override]
     protected function getPackageProviders(mixed $app): array {
         return array_merge(parent::getPackageProviders($app), [
-            Provider::class,
+            PackageProvider::class,
             CoreProvider::class,
             SerializerProvider::class,
         ]);
