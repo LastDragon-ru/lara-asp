794a843f49c0d3042c6b3a46585ee66ac1657135
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2024-12-10 10:59:52 +0400

    Extracted `\LastDragon_ru\LaraASP\Documentator\Markdown\Extensions\Generated\Extension`.

---
+++ packages/documentator/src/Markdown/Extensions/Generated/Extension.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Documentator\Markdown\Extensions\Generated;
+
+use League\CommonMark\Environment\EnvironmentBuilderInterface;
+use League\CommonMark\Extension\ExtensionInterface;
+use Override;
+
+/**
+ * Generated block parser.
+ *
+ * ```markdown
+ * [//]: # (start: block)
+ *
+ * Block content.
+ *
+ * [//]: # (end: block)
+ * ```
+ */
+class Extension implements ExtensionInterface {
+    #[Override]
+    public function register(EnvironmentBuilderInterface $environment): void {
+        $environment
+            ->addBlockStartParser(new ParserStart(), 100);
+    }
+}
