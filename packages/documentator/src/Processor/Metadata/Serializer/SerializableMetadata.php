926cbd3096c61c9a3772ef6285792cbc4faafbc9
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2025-02-09 09:14:17 +0400

    feat(documentator): Added built-in metadata for `\LastDragon_ru\LaraASP\Serializer\Contracts\Serializable`.

---
+++ packages/documentator/src/Processor/Metadata/Serializer/SerializableMetadata.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Documentator\Processor\Metadata\Serializer;
+
+use LastDragon_ru\LaraASP\Core\Path\FilePath;
+use LastDragon_ru\LaraASP\Documentator\Processor\Contracts\MetadataSerializer;
+use LastDragon_ru\LaraASP\Documentator\Processor\FileSystem\File;
+use LastDragon_ru\LaraASP\Documentator\Processor\Metadata\FileSystem\Content;
+use LastDragon_ru\LaraASP\Serializer\Contracts\Serializable;
+use LastDragon_ru\LaraASP\Serializer\Contracts\Serializer;
+use Override;
+
+use function is_a;
+
+/**
+ * @implements MetadataSerializer<Serializable>
+ */
+readonly class SerializableMetadata implements MetadataSerializer {
+    public function __construct(
+        protected Serializer $serializer,
+    ) {
+        // empty
+    }
+
+    /**
+     * @inheritDoc
+     */
+    #[Override]
+    public static function getExtensions(): array {
+        return ['json'];
+    }
+
+    #[Override]
+    public function isSupported(string $metadata): bool {
+        return is_a($metadata, Serializable::class, true);
+    }
+
+    #[Override]
+    public function resolve(File $file, string $metadata): mixed {
+        return $this->serializer->deserialize($metadata, $file->as(Content::class)->content, $file->getExtension());
+    }
+
+    #[Override]
+    public function serialize(FilePath $path, object $value): string {
+        return $this->serializer->serialize($value, $path->getExtension());
+    }
+}
---
+++ packages/documentator/src/Processor/Metadata/Serializer/SerializableMetadataTest.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Documentator\Processor\Metadata\Serializer;
+
+use LastDragon_ru\LaraASP\Core\Path\FilePath;
+use LastDragon_ru\LaraASP\Documentator\Processor\FileSystem\File;
+use LastDragon_ru\LaraASP\Documentator\Processor\Metadata\FileSystem\Content;
+use LastDragon_ru\LaraASP\Documentator\Testing\Package\TestCase;
+use LastDragon_ru\LaraASP\Serializer\Contracts\Serializable;
+use LastDragon_ru\LaraASP\Serializer\Contracts\Serializer;
+use Mockery;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+/**
+ * @internal
+ */
+#[CoversClass(SerializableMetadata::class)]
+final class SerializableMetadataTest extends TestCase {
+    public function testResolve(): void {
+        $object     = Mockery::mock(Serializable::class);
+        $content    = 'content';
+        $extension  = 'json';
+        $serializer = Mockery::mock(Serializer::class);
+        $serializer
+            ->shouldReceive('deserialize')
+            ->with(Serializable::class, $content, $extension)
+            ->once()
+            ->andReturn($object);
+
+        $file = Mockery::mock(File::class);
+        $file
+            ->shouldReceive('getExtension')
+            ->once()
+            ->andReturn($extension);
+        $file
+            ->shouldReceive('as')
+            ->with(Content::class)
+            ->once()
+            ->andReturn(new Content($content));
+
+        $metadata = new SerializableMetadata($serializer);
+        $actual   = $metadata->resolve($file, Serializable::class);
+
+        self::assertSame($object, $actual);
+    }
+
+    public function testSerialize(): void {
+        $object     = Mockery::mock(Serializable::class);
+        $content    = 'content';
+        $extension  = 'json';
+        $serializer = Mockery::mock(Serializer::class);
+        $serializer
+            ->shouldReceive('serialize')
+            ->with($object, $extension)
+            ->once()
+            ->andReturn($content);
+
+        $metadata = new SerializableMetadata($serializer);
+        $actual   = $metadata->serialize(new FilePath("file.{$extension}"), $object);
+
+        self::assertSame($content, $actual);
+    }
+}
