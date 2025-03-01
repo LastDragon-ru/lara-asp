a1838f4c548b4c3fc4b74dfd1acb1c1f8f8d5842
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2025-03-01 13:39:40 +0400

    New metadata `\LastDragon_ru\LaraASP\Documentator\Processor\Metadata\Context\IndexFile`.

--- packages/documentator/src/Processor/ProcessorTest.php
+++ packages/documentator/src/Processor/ProcessorTest.php
@@ -6,10 +6,9 @@ use Generator;
 use LastDragon_ru\LaraASP\Core\Application\ContainerResolver;
 use LastDragon_ru\LaraASP\Core\Path\DirectoryPath;
 use LastDragon_ru\LaraASP\Core\Path\FilePath;
-use LastDragon_ru\LaraASP\Core\Utils\Cast;
 use LastDragon_ru\LaraASP\Documentator\Processor\Contracts\Dependency;
+use LastDragon_ru\LaraASP\Documentator\Processor\Contracts\MetadataResolver;
 use LastDragon_ru\LaraASP\Documentator\Processor\Contracts\Task;
-use LastDragon_ru\LaraASP\Documentator\Processor\Dependencies\Context;
 use LastDragon_ru\LaraASP\Documentator\Processor\Dependencies\FileReference;
 use LastDragon_ru\LaraASP\Documentator\Processor\Events\DependencyResolved;
 use LastDragon_ru\LaraASP\Documentator\Processor\Events\DependencyResolvedResult;
@@ -27,6 +26,8 @@ use LastDragon_ru\LaraASP\Documentator\Processor\Exceptions\DependencyCircularDe
 use LastDragon_ru\LaraASP\Documentator\Processor\Exceptions\DependencyUnresolvable;
 use LastDragon_ru\LaraASP\Documentator\Processor\FileSystem\File;
 use LastDragon_ru\LaraASP\Documentator\Processor\FileSystem\Hook;
+use LastDragon_ru\LaraASP\Documentator\Processor\Metadata\Context;
+use LastDragon_ru\LaraASP\Documentator\Processor\Metadata\Context\IndexFile;
 use LastDragon_ru\LaraASP\Documentator\Testing\Package\TestCase;
 use Mockery;
 use Override;
@@ -34,7 +35,6 @@ use PHPUnit\Framework\Attributes\CoversClass;
 use stdClass;
 
 use function array_map;
-use function is_string;
 
 /**
  * @internal
@@ -323,82 +323,41 @@ final class ProcessorTest extends TestCase {
     }
 
     public function testRunContext(): void {
-        $taskA   = new class() extends ProcessorTest__Task {
-            #[Override]
-            public function __invoke(File $file): Generator {
-                // Context dependency
-                $context    = Cast::to(File::class, yield new Context());
-                $dependency = $context->as(stdClass::class);
+        $input     = new FilePath('index.md');
+        $context   = new stdClass();
+        $processor = Mockery::mock(Processor::class, [Mockery::mock(ContainerResolver::class)]);
+        $processor->shouldAllowMockingProtectedMethods();
+        $processor->makePartial();
+        $processor
+            ->shouldReceive('execute')
+            ->once()
+            ->andReturn();
+        $processor
+            ->shouldReceive('addMetadata')
+            ->with(
+                Mockery::on(static function (MetadataResolver $resolver) use ($input, $context): bool {
+                    self::assertEquals(
+                        new Context([
+                            new IndexFile($input),
+                            $context,
+                        ]),
+                        $resolver,
+                    );
 
-                if (isset($dependency->path) && is_string($dependency->path)) {
-                    $resolved          = Cast::to(File::class, yield new FileReference($dependency->path));
-                    $this->processed[] = [(string) $file, [$dependency->path => (string) $resolved]];
-                }
-
-                // Parent
-                yield from parent::__invoke($file);
-
-                // Return
-                return true;
-            }
-        };
-        $input   = (new FilePath(self::getTestData()->path('c.txt')))->getNormalizedPath();
-        $events  = [];
-        $context = new class('c.html') extends stdClass {
-            public function __construct(
-                public string $path,
-            ) {
-                // empty
-            }
-        };
-
-        (new Processor($this->app()->make(ContainerResolver::class)))
-            ->addTask($taskA)
-            ->addListener(
-                static function (Event $event) use (&$events): void {
-                    $events[] = $event;
-                },
+                    return true;
+                }),
             )
-            ->run(
-                input  : $input,
-                context: [$context],
-            );
+            ->once()
+            ->andReturn();
+        $processor
+            ->shouldReceive('removeMetadata')
+            ->with(Context::class)
+            ->once()
+            ->andReturn();
 
-        self::assertEquals(
-            [
-                new ProcessingStarted(),
-                new FileStarted('@ :before'),
-                new FileFinished(FileFinishedResult::Skipped),
-                new FileStarted('↔ c.txt'),
-                new TaskStarted($taskA::class),
-                new DependencyResolved('@ :context', DependencyResolvedResult::Success),
-                new DependencyResolved('↔ c.html', DependencyResolvedResult::Success),
-                new FileStarted('↔ c.html'),
-                new FileFinished(FileFinishedResult::Skipped),
-                new TaskFinished(TaskFinishedResult::Success),
-                new FileFinished(FileFinishedResult::Success),
-                new FileStarted('@ :after'),
-                new FileFinished(FileFinishedResult::Skipped),
-                new ProcessingFinished(ProcessingFinishedResult::Success),
-            ],
-            $events,
-        );
-        self::assertEquals(
-            [
-                [
-                    (string) $input->getFilePath('c.txt'),
-                    [
-                        'c.html' => (string) $input->getFilePath('c.html'),
-                    ],
-                ],
-                [
-                    (string) $input->getFilePath('c.txt'),
-                    [
-                        // empty (from parent)
-                    ],
-                ],
-            ],
-            $taskA->processed,
+        $processor->run(
+            input  : $input,
+            context: [$context],
         );
     }
 
