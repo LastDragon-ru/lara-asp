6bb9aadc452cadb684e8a16737d1c94a2c5348a2
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2025-03-17 14:42:59 +0400

    `Mutator` implementation.

---
+++ packages/documentator/src/Markdown/Mutator/Mutation.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Documentator\Markdown\Mutator;
+
+use LastDragon_ru\LaraASP\Documentator\Markdown\Document;
+use LastDragon_ru\LaraASP\Documentator\Markdown\Mutator\Mutagens\Delete;
+use LastDragon_ru\LaraASP\Documentator\Markdown\Mutator\Mutagens\Extract;
+use LastDragon_ru\LaraASP\Documentator\Markdown\Mutator\Mutagens\Finalize;
+use LastDragon_ru\LaraASP\Documentator\Markdown\Mutator\Mutagens\Replace;
+use League\CommonMark\Node\Node;
+
+/**
+ * Must not modify original {@see Document}/{@see Node}.
+ *
+ * @template TNode of Node
+ */
+interface Mutation {
+    /**
+     * @return list<class-string<TNode>>
+     */
+    public static function nodes(): array;
+
+    /**
+     * @param TNode $node
+     *
+     * @return list<Replace|Delete|Extract|Finalize>
+     */
+    public function mutagens(Document $document, Node $node): array;
+}
