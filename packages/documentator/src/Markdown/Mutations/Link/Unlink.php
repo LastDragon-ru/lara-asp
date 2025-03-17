2ea96ac6051488ca75ffe5e6ba94b8bb6044b3a2
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2025-03-17 16:37:16 +0400

    `\LastDragon_ru\LaraASP\Documentator\Markdown\Mutations\Link\Unlink` and `\LastDragon_ru\LaraASP\Documentator\Markdown\Mutations\Link\UnlinkToSelf` updated.

--- packages/documentator/src/Markdown/Mutations/Link/Unlink.php
+++ packages/documentator/src/Markdown/Mutations/Link/Unlink.php
@@ -2,34 +2,28 @@
 
 namespace LastDragon_ru\LaraASP\Documentator\Markdown\Mutations\Link;
 
-use LastDragon_ru\LaraASP\Documentator\Markdown\Contracts\Mutation;
 use LastDragon_ru\LaraASP\Documentator\Markdown\Data\Content;
 use LastDragon_ru\LaraASP\Documentator\Markdown\Data\Location;
 use LastDragon_ru\LaraASP\Documentator\Markdown\Document;
+use LastDragon_ru\LaraASP\Documentator\Markdown\Mutator\Mutagens\Delete;
+use League\CommonMark\Node\Node;
 use Override;
 
 /**
  * Unlink all links.
  */
-readonly class Unlink extends Base implements Mutation {
+readonly class Unlink extends Base {
     /**
      * @inheritDoc
      */
     #[Override]
-    public function __invoke(Document $document): iterable {
-        // Just in case
-        yield from [];
+    public function mutagens(Document $document, Node $node): array {
+        $location = Location::get($node);
+        $content  = Content::get($node);
 
-        // Update
-        foreach ($this->nodes($document) as $node) {
-            $location = Location::get($node);
-            $content  = Content::get($node);
-
-            yield [$location->withLength(1), null];                                                               // [
-            yield [$location->withOffset(($content->offset - $location->offset) + (int) $content->length), null]; // ](...)
-        }
-
-        // Return
-        return true;
+        return [
+            new Delete($location->withLength(1)),
+            new Delete($location->withOffset(($content->offset - $location->offset) + (int) $content->length)),
+        ];
     }
 }
