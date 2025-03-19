dd8547a5aecfaf2203b3e694f6d9191106badf57
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2025-03-19 08:55:51 +0400

    `\LastDragon_ru\LaraASP\Documentator\Markdown\Mutations\Document\MakeInlinable` and `\LastDragon_ru\LaraASP\Documentator\Markdown\Mutations\Document\MakeSplittable` updated.

--- packages/documentator/src/Markdown/Mutations/Document/MakeInlinable.php
+++ packages/documentator/src/Markdown/Mutations/Document/MakeInlinable.php
@@ -2,19 +2,35 @@
 
 namespace LastDragon_ru\LaraASP\Documentator\Markdown\Mutations\Document;
 
-use LastDragon_ru\LaraASP\Documentator\Markdown\Mutations\CompositeMutation;
+use Generator;
+use IteratorAggregate;
 use LastDragon_ru\LaraASP\Documentator\Markdown\Mutations\Footnote\Prefix as FootnotesPrefix;
 use LastDragon_ru\LaraASP\Documentator\Markdown\Mutations\Reference\Prefix as ReferencesPrefix;
+use LastDragon_ru\LaraASP\Documentator\Markdown\Mutator\Mutation;
+use League\CommonMark\Node\Node;
+use Override;
 
 /**
  * Renames all references/footnotes/etc to make possible inline the
  * document into another document without conflicts/ambiguities.
+ *
+ * @implements IteratorAggregate<array-key, Mutation<covariant Node>>
  */
-readonly class MakeInlinable extends CompositeMutation {
-    public function __construct(string $prefix) {
-        parent::__construct(
-            new FootnotesPrefix($prefix),
-            new ReferencesPrefix($prefix),
-        );
+readonly class MakeInlinable implements IteratorAggregate {
+    public function __construct(
+        protected string $prefix,
+    ) {
+        // empty
+    }
+
+    /**
+     * @return Generator<array-key, Mutation<covariant Node>>
+     */
+    #[Override]
+    public function getIterator(): Generator {
+        yield from [
+            new FootnotesPrefix($this->prefix),
+            new ReferencesPrefix($this->prefix),
+        ];
     }
 }
