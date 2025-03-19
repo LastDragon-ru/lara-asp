83d90497c12454fd9d49c25b92aa6806b0074afe
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2025-03-19 08:57:02 +0400

    `\LastDragon_ru\LaraASP\Documentator\Processor\Tasks\Preprocess\Mutations\InstructionsRemove` updated.

--- packages/documentator/src/Processor/Tasks/Preprocess/Mutations/InstructionsRemove.php
+++ packages/documentator/src/Processor/Tasks/Preprocess/Mutations/InstructionsRemove.php
@@ -2,18 +2,22 @@
 
 namespace LastDragon_ru\LaraASP\Documentator\Processor\Tasks\Preprocess\Mutations;
 
-use LastDragon_ru\LaraASP\Documentator\Markdown\Contracts\Mutation;
 use LastDragon_ru\LaraASP\Documentator\Markdown\Data\Location;
 use LastDragon_ru\LaraASP\Documentator\Markdown\Document;
+use LastDragon_ru\LaraASP\Documentator\Markdown\Extensions\Reference\Node as ReferenceNode;
+use LastDragon_ru\LaraASP\Documentator\Markdown\Mutator\Mutagens\Delete;
+use LastDragon_ru\LaraASP\Documentator\Markdown\Mutator\Mutation;
 use LastDragon_ru\LaraASP\Documentator\Processor\Tasks\Preprocess\Instructions;
 use LastDragon_ru\LaraASP\Documentator\Processor\Tasks\Preprocess\Utils;
-use League\CommonMark\Node\NodeIterator;
+use League\CommonMark\Node\Node;
 use Override;
 
 /**
  * Removes all instructions.
  *
  * @internal
+ *
+ * @implements Mutation<ReferenceNode>
  */
 readonly class InstructionsRemove implements Mutation {
     public function __construct(
@@ -26,19 +30,19 @@ readonly class InstructionsRemove implements Mutation {
      * @inheritDoc
      */
     #[Override]
-    public function __invoke(Document $document): iterable {
-        // Just in case
-        yield from [];
+    public static function nodes(): array {
+        return [
+            ReferenceNode::class,
+        ];
+    }
 
-        // Update
-        foreach ($document->node->iterator(NodeIterator::FLAG_BLOCKS_ONLY) as $node) {
-            // Instruction?
-            if (!Utils::isInstruction($node, $this->instructions)) {
-                continue;
-            }
-
-            // Change
-            yield [Location::get($node), null];
-        }
+    /**
+     * @inheritDoc
+     */
+    #[Override]
+    public function mutagens(Document $document, Node $node): array {
+        return Utils::isInstruction($node, $this->instructions)
+            ? [new Delete(Location::get($node))]
+            : [];
     }
 }
