afc6bb967f8b54990d288bb2c14decb041e8a334
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2025-04-23 09:23:48 +0400

    feat(documentator/Markdown): New mutation `\LastDragon_ru\LaraASP\Documentator\Markdown\Mutations\Link\AutolinkExpand`.

---
+++ packages/documentator/src/Markdown/Mutations/Link/AutolinkExpand.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Documentator\Markdown\Mutations\Link;
+
+use LastDragon_ru\LaraASP\Documentator\Markdown\Contracts\Document;
+use LastDragon_ru\LaraASP\Documentator\Markdown\Data\Content;
+use LastDragon_ru\LaraASP\Documentator\Markdown\Data\Location;
+use LastDragon_ru\LaraASP\Documentator\Markdown\Mutator\Mutagens\Replace;
+use LastDragon_ru\LaraASP\Documentator\Markdown\Utils;
+use League\CommonMark\Node\Node;
+use Override;
+
+use function mb_substr;
+use function rawurldecode;
+use function str_starts_with;
+
+/**
+ * Converts autolink into normal link.
+ *
+ * @since %{VERSION}
+ */
+readonly class AutolinkExpand extends Base {
+    /**
+     * @inheritDoc
+     */
+    #[Override]
+    public function mutagens(Document $document, Node $node): array {
+        // Autolink?
+        $content = Content::optional()->get($node);
+
+        if ($content !== null) {
+            return [];
+        }
+
+        // Convert
+        $url      = rawurldecode($node->getUrl());
+        $label    = str_starts_with($url, 'mailto:') ? mb_substr($url, 7) : $url;
+        $label    = Utils::escapeTextInTableCell($node, $label);
+        $target   = Utils::getLinkTarget($node, $url);
+        $content  = "[{$label}]({$target})";
+        $location = Location::get($node);
+
+        return [
+            new Replace($location, $content),
+        ];
+    }
+}
---
+++ packages/documentator/src/Markdown/Mutations/Link/AutolinkExpandTest.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\LaraASP\Documentator\Markdown\Mutations\Link;
+
+use LastDragon_ru\LaraASP\Core\Path\FilePath;
+use LastDragon_ru\LaraASP\Documentator\Markdown\Contracts\Markdown;
+use LastDragon_ru\LaraASP\Documentator\Testing\Package\TestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+/**
+ * @internal
+ */
+#[CoversClass(AutolinkExpand::class)]
+#[CoversClass(Base::class)]
+final class AutolinkExpandTest extends TestCase {
+    public function testInvoke(): void {
+        $content = <<<'MARKDOWN'
+            # Header
+
+            Text text <https://example.com> text text text text
+            text <https://example.com/ /path/to/something> text
+            text text <example@example.com>.
+
+            ## Inside Table
+
+            | Header A                                 | Header B                                |
+            |------------------------------------------|-----------------------------------------|
+            | Cell <https://example.com/\|/path> cell. | <example@example.com>                   |
+            | Cell                                     | <https://example.com/%20/path/in/cell>. |
+            MARKDOWN;
+
+        $markdown = $this->app()->make(Markdown::class);
+        $document = $markdown->parse($content, new FilePath('path/to/file.md'));
+        $actual   = (string) $document->mutate(new AutolinkExpand());
+
+        self::assertSame(
+            <<<'MARKDOWN'
+            # Header
+
+            Text text [https://example.com](https://example.com) text text text text
+            text <https://example.com/ /path/to/something> text
+            text text [example@example.com](mailto:example@example.com).
+
+            ## Inside Table
+
+            | Header A                                 | Header B                                |
+            |------------------------------------------|-----------------------------------------|
+            | Cell [https://example.com/\|/path](https://example.com/%7C/path) cell. | [example@example.com](mailto:example@example.com)                   |
+            | Cell                                     | [https://example.com/ /path/in/cell](<https://example.com/ /path/in/cell>). |
+
+            MARKDOWN,
+            $actual,
+        );
+    }
+}
