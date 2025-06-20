f819101f2404fbbd650d600f64d0c0e66c1aa7ab
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2025-06-20 09:37:54 +0400

    Streams to parse strings.

--- packages/parser/src/Package.php
+++ packages/parser/src/Package.php
@@ -3,5 +3,10 @@
 namespace LastDragon_ru\LaraASP\Parser;
 
 final class Package {
-    public const string Name = 'lara-asp-parser';
+    public const string Name     = 'lara-asp-parser';
+    public const string Encoding = 'UTF-8';
+    /**
+     * @internal
+     */
+    public const int BufferCharacters = 8_192;
 }
