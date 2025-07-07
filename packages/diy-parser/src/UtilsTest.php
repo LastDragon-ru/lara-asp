30c82b82dbd537c46d5e488e0909645ced1ee722
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2025-07-07 11:16:08 +0400

    Added `\LastDragon_ru\LaraASP\Parser\Utils::toString()` (= `implode()`)

---
+++ packages/diy-parser/src/UtilsTest.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\DiyParser;
+
+use LastDragon_ru\DiyParser\Testing\Package\TestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+/**
+ * @internal
+ */
+#[CoversClass(Utils::class)]
+final class UtilsTest extends TestCase {
+    public function testToString(): void {
+        self::assertSame('abc', Utils::toString(['a', 'b', 'c']));
+        self::assertSame('a,b,c', Utils::toString(['a', 'b', 'c'], ','));
+    }
+}
