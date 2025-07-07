e1cf31046b4d0a084034bd95cf057eb7137f0270
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2025-07-07 09:57:36 +0400

    Added `\LastDragon_ru\LaraASP\Parser\Iterables\TransformIterable` (= `array_map()`).

---
+++ packages/diy-parser/src/Iterables/TransformIterableTest.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\DiyParser\Iterables;
+
+use LastDragon_ru\DiyParser\Testing\Package\TestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+use function iterator_to_array;
+
+/**
+ * @internal
+ */
+#[CoversClass(TransformIterable::class)]
+final class TransformIterableTest extends TestCase {
+    public function testGetIterator(): void {
+        self::assertSame(
+            [2, 4, 6, 8, 10],
+            iterator_to_array(new TransformIterable([1, 2, 3, 4, 5], static fn ($v) => 2 * $v), false),
+        );
+    }
+}
