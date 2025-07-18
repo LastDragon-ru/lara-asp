b470ce5f102ce04d8e57c400cf8b8196f67bb9c3
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2025-07-18 15:12:41 +0400

    Added `\LastDragon_ru\LaraASP\Parser\Ast\NodeString` default implementation.

---
+++ packages/diy-parser/src/Ast/NodeStringTest.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\DiyParser\Ast;
+
+use LastDragon_ru\DiyParser\Testing\Package\TestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+/**
+ * @internal
+ */
+#[CoversClass(NodeString::class)]
+final class NodeStringTest extends TestCase {
+    public function testMerge(): void {
+        $a = new NodeString('1');
+        $b = new NodeString('2');
+        $c = new class('3') extends NodeString {
+            // empty
+        };
+
+        self::assertSame($a, NodeString::merge($a, $b));
+        self::assertSame('12', $a->string);
+
+        self::assertSame($c, NodeString::merge($b, $c));
+        self::assertSame('2', $b->string);
+        self::assertSame('3', $c->string);
+    }
+}
