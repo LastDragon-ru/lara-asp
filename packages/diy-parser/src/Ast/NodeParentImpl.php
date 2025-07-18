00b51396b3d6e901c608cf9f7103487017bfbe07
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2025-07-18 10:34:54 +0400

    Default implementation for `\LastDragon_ru\LaraASP\Parser\Ast\NodeParent`.

---
+++ packages/diy-parser/src/Ast/NodeParentImpl.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\DiyParser\Ast;
+
+use Override;
+use Traversable;
+
+use function array_splice;
+use function count;
+
+/**
+ * @template TChild of NodeChild
+ *
+ * @implements NodeParent<TChild>
+ */
+abstract class NodeParentImpl implements NodeParent {
+    public function __construct(
+        /**
+         * @var list<TChild>
+         */
+        public array $children,
+    ) {
+        // empty
+    }
+
+    #[Override]
+    public function count(): int {
+        return count($this->children);
+    }
+
+    #[Override]
+    public function getIterator(): Traversable {
+        yield from $this->children;
+    }
+
+    #[Override]
+    public function offsetExists(mixed $offset): bool {
+        return isset($this->children[$offset]);
+    }
+
+    #[Override]
+    public function offsetGet(mixed $offset): mixed {
+        return $this->children[$offset] ?? null;
+    }
+
+    #[Override]
+    public function offsetSet(mixed $offset, mixed $value): void {
+        array_splice($this->children, $offset ?? count($this->children), 1, [$value]);
+    }
+
+    #[Override]
+    public function offsetUnset(mixed $offset): void {
+        array_splice($this->children, $offset, 1);
+    }
+}
---
+++ packages/diy-parser/src/Ast/NodeParentImplTest.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\DiyParser\Ast;
+
+use LastDragon_ru\DiyParser\Testing\Package\TestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+use function iterator_to_array;
+
+/**
+ * @internal
+ */
+#[CoversClass(NodeParentImpl::class)]
+final class NodeParentImplTest extends TestCase {
+    public function testCountable(): void {
+        $child  = new ParentNodeTest_ChildNode();
+        $parent = new ParentNodeTest_ParentNode([$child]);
+
+        self::assertCount(1, $parent);
+    }
+
+    public function testArrayAccess(): void {
+        $aChild = new ParentNodeTest_ChildNode();
+        $bChild = new ParentNodeTest_ChildNode();
+        $cChild = new ParentNodeTest_ChildNode();
+        $parent = new ParentNodeTest_ParentNode([$aChild]);
+
+        self::assertSame($aChild, $parent[0]);
+        self::assertNull($parent[1]);
+
+        $parent[]  = $bChild;
+        $parent[2] = $cChild;
+
+        self::assertSame($bChild, $parent[1]);
+        self::assertSame($cChild, $parent[2]);
+
+        $parent[1] = $cChild;
+
+        self::assertSame($cChild, $parent[1]);
+
+        unset($parent[0]);
+
+        self::assertEquals(
+            [$cChild, $cChild],
+            iterator_to_array($parent, false),
+        );
+    }
+
+    public function testIteratorAggregate(): void {
+        $aChild = new ParentNodeTest_ChildNode();
+        $bChild = new ParentNodeTest_ChildNode();
+        $parent = new ParentNodeTest_ParentNode([$aChild, $bChild]);
+
+        self::assertEquals([$aChild, $bChild], iterator_to_array($parent, false));
+    }
+}
+
+// @phpcs:disable PSR1.Classes.ClassDeclaration.MultipleClasses
+// @phpcs:disable Squiz.Classes.ValidClassName.NotCamelCaps
+
+/**
+ * @internal
+ * @implements NodeChild<ParentNodeTest_ParentNode>
+ */
+class ParentNodeTest_ChildNode implements NodeChild {
+    // empty
+}
+
+/**
+ * @internal
+ * @extends NodeParentImpl<ParentNodeTest_ChildNode>
+ */
+class ParentNodeTest_ParentNode extends NodeParentImpl {
+    // empty
+}
