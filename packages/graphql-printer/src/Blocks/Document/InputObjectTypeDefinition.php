d4c9375d9bd124979c3b3fb78cf42abd1657d962
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2025-07-29 15:12:23 +0400

    feat(graphql-printer): `@oneOf` support.

--- packages/graphql-printer/src/Blocks/Document/InputObjectTypeDefinition.php
+++ packages/graphql-printer/src/Blocks/Document/InputObjectTypeDefinition.php
@@ -2,7 +2,12 @@
 
 namespace LastDragon_ru\LaraASP\GraphQLPrinter\Blocks\Document;
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use GraphQL\Language\AST\InputObjectTypeDefinitionNode;
+use GraphQL\Language\AST\NodeList;
+use GraphQL\Language\Parser;
+use GraphQL\Type\Definition\Directive;
 use GraphQL\Type\Definition\InputObjectType;
 use LastDragon_ru\LaraASP\GraphQLPrinter\Blocks\Types\InputObjectDefinitionBlock;
 use LastDragon_ru\LaraASP\GraphQLPrinter\Misc\Context;
@@ -28,5 +33,36 @@ class InputObjectTypeDefinition extends InputObjectDefinitionBlock {
     #[Override]
     protected function prefix(): ?string {
         return 'input';
+    }
+
+    #[Override]
+    protected function getDefinitionDirectives(): NodeList {
+        // Is `@oneOf` supported?
+        $definition            = $this->getDefinition();
+        $directives            = parent::getDefinitionDirectives();
+        static $oneOfSupported = InstalledVersions::satisfies(new VersionParser(), 'webonyx/graphql-php', '>=15.21.0');
+
+        if (!$oneOfSupported || !($definition instanceof InputObjectType) || !$definition->isOneOf) {
+            return $directives;
+        }
+
+        // Add
+        $oneOfName      = Directive::ONE_OF_NAME;
+        $oneOfDirective = Parser::directive("@{$oneOfName}");
+
+        foreach ($directives as $key => $directive) {
+            if ($directive->name->value === $oneOfName) {
+                $directives[$key] = $oneOfDirective;
+                $oneOfDirective   = null;
+                break;
+            }
+        }
+
+        if ($oneOfDirective !== null) {
+            $directives[] = $oneOfDirective;
+        }
+
+        // Return
+        return $directives;
     }
 }
