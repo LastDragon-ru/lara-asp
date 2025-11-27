7a6d1d8eb90b4633a64a0516f0fa244cc1aba2f0
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2025-11-27 21:07:27 +0400

    feat(serializer): New `\LastDragon_ru\LaraASP\Serializer\Normalizers\EnumNormalizer` for `\UnitEnum` and `\BackedEnum`.

--- packages/serializer/src/FactoryTest.php
+++ packages/serializer/src/FactoryTest.php
@@ -6,8 +6,8 @@ use LastDragon_ru\LaraASP\Core\Application\ContainerResolver;
 use LastDragon_ru\LaraASP\Serializer\Config\Config;
 use LastDragon_ru\LaraASP\Serializer\Contracts\Serializer;
 use LastDragon_ru\LaraASP\Serializer\Normalizers\DateTimeNormalizer;
+use LastDragon_ru\LaraASP\Serializer\Normalizers\EnumNormalizer;
 use LastDragon_ru\LaraASP\Serializer\Normalizers\SerializableNormalizer;
-use LastDragon_ru\LaraASP\Serializer\Normalizers\UnitEnumNormalizer;
 use LastDragon_ru\LaraASP\Serializer\Package\TestCase;
 use Mockery;
 use PHPUnit\Framework\Attributes\CoversClass;
@@ -15,7 +15,6 @@ use Symfony\Component\Serializer\Encoder\CsvEncoder;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
-use Symfony\Component\Serializer\Normalizer\BackedEnumNormalizer;
 use Symfony\Component\Serializer\Normalizer\DataUriNormalizer;
 use Symfony\Component\Serializer\Normalizer\DateIntervalNormalizer;
 use Symfony\Component\Serializer\Normalizer\DateTimeZoneNormalizer;
@@ -65,18 +64,16 @@ final class FactoryTest extends TestCase {
                     self::assertSame('format from config', $format);
                     self::assertEquals(
                         [
-                            'encoder option from config'                  => 'encoder',
-                            'encoder option'                              => 'call',
-                            'normalizer option from config'               => 'normalizer',
-                            'normalizer option'                           => 'call',
-                            'context option from config'                  => 'context',
-                            'context option'                              => 'call',
-                            'csv encoder option'                          => 'call',
-                            'json_encode_options'                         => 4_197_698,
-                            'json_decode_options'                         => 4_194_304,
-                            'allow_invalid_values'                        => false,
-                            DateTimeNormalizer::ContextFormat             => 'Y-m-d\TH:i:s.vP',
-                            UnitEnumNormalizer::ContextAllowInvalidValues => false,
+                            'encoder option from config'      => 'encoder',
+                            'encoder option'                  => 'call',
+                            'normalizer option from config'   => 'normalizer',
+                            'normalizer option'               => 'call',
+                            'context option from config'      => 'context',
+                            'context option'                  => 'call',
+                            'csv encoder option'              => 'call',
+                            'json_encode_options'             => 4_197_698,
+                            'json_decode_options'             => 4_194_304,
+                            DateTimeNormalizer::ContextFormat => 'Y-m-d\TH:i:s.vP',
                         ],
                         $context,
                     );
@@ -94,8 +91,7 @@ final class FactoryTest extends TestCase {
                             DataUriNormalizer::class,
                             ArrayDenormalizer::class,
                             DateIntervalNormalizer::class,
-                            UnitEnumNormalizer::class,
-                            BackedEnumNormalizer::class,
+                            EnumNormalizer::class,
                         ],
                         $normalizers,
                     );
