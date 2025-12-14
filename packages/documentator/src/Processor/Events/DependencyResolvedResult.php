efdd6c4d9d89f501ffa50059d9db1e1888d03d23
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2025-12-14 19:24:40 +0400

    Dead code removal.

--- packages/documentator/src/Processor/Events/DependencyResolvedResult.php
+++ packages/documentator/src/Processor/Events/DependencyResolvedResult.php
@@ -4,7 +4,6 @@ namespace LastDragon_ru\LaraASP\Documentator\Processor\Events;
 
 enum DependencyResolvedResult {
     case Success;
-    case Missed;
     case Failed;
     case Null;
     case Queued;
